8ec80f2f99a4a5760ae438b34209a37cdd24a25c
Erik Osheim <d_m@plastic-idolatry.com>
2012-01-14 17:16:02 -0500

    remove misguided Int/Float pow

--- src/main/scala/numerics/package.scala
+++ src/main/scala/numerics/package.scala
@@ -3,13 +3,7 @@ package numerics.math
 import scala.annotation.tailrec
 
 object fun {
-
-  /**
-   *
-   */
-  final def pow(base:Int, exponent:Int):Int = {
-    pow(base.toLong, exponent.toLong).toInt
-  }
+  // TODO: add nroot(base:Long, n:Long)
 
   /**
    * Exponentiation function, e.g. x^y
@@ -17,19 +11,27 @@ object fun {
    * If base^exponent doesn't fit in a Long, the result will overflow (unlike
    * scala.math.pow which will return +/- Infinity). 
    */
-  final def pow(base:Long, exponent:Long):Long = {
-    if (exponent < 0L) sys.error("illegal exponent")
+  final def pow(base:Long, exponent:Long):Long = if (exponent < 0L) {
+    _inv_pow(base, exponent)
+  } else {
     _pow(1L, base, exponent)
+  }
+
+  // inverse powers for integers will return -1L, 0L, 1L, or throw an error.
+  private final def _inv_pow(base:Long, exponent:Long) = if(base == 0L) {
+    throw new Exception("zero can't be raised to negative power")
+  } else if (base == 1L) {
+    1L
+  } else if (base == -1L) {
+    if (exponent % 2 == 0) -1L else 1L
+  } else {
+    0L
   }
 
   // tail-recursive helper method for pow(Long, Long)
   @tailrec private final def _pow(t:Long, b:Long, e:Long): Long = {
     if (e == 0L) return t
     _pow(if (e % 2 == 1L) t * b else t, b * b, e / 2L)
-  }
-
-  final def pow(base:Float, exponent:Float):Float = {
-    java.lang.Math.pow(base.toDouble, exponent.toDouble).toFloat
   }
 
   final def pow(base:Double, exponent:Double):Double = {
