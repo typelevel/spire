d6c0df5e685238fad104644fed508392cbe1bf3b
Tom Switzer <thomas.switzer@gmail.com>
2012-04-10 12:14:23 -0400

    Remove sign(um) and abs from Ring, add new Signed type class.
    Adds a new Signed[A] type class for working with signs. Numeric,
    Fractional, Integral all extend it, but Ring, EuclideanRing, and Field
    do not. Also adds tests, which found a bug in Real (commit fixes this
    bug).

---
+++ src/test/scala/spire/math/SignedTest.scala
@@ -0,0 +1,52 @@
+package spire.math
+
+// scalatest
+import org.scalatest.FunSuite
+
+// we need to disable our own === to avoid messing up ScalaTest.
+import Implicits.{eqOps => _, _}
+
+// nice alias
+import scala.{specialized => spec}
+
+import java.math.MathContext
+
+import spire.math.fpf.FPFilter
+
+
+class SignedTest extends FunSuite {
+  def runWith[@spec(Int, Long, Float, Double) A: Signed: Manifest](neg: A, pos: A, zero: A) {
+    val m = implicitly[Manifest[A]]
+
+    // the name to use for this A
+    val cls = m.typeArguments match {
+      case Nil => m.erasure.getSimpleName
+      case args => "%s[%s]" format (m.erasure.getSimpleName, args.mkString(","))
+    }
+
+    // test runner which constructs a unique name for each test we run.
+    def runTest(name:String)(f: => Unit) = test("%s:%s" format(cls, name))(f)
+
+    runTest("-neg.abs === pos")(assert(neg.abs === pos))
+    runTest("pos.abs === pos")(assert(pos.abs === pos))
+    runTest("neg.sign == Negative")(assert(neg.sign === Negative))
+    runTest("pos.sign == Positive")(assert(pos.sign === Positive))
+    runTest("zero.sign == Zero")(assert(zero.sign === Zero))
+    runTest("neg.signum < 0")(assert(neg.signum < 0))
+    runTest("pos.signum > 0")(assert(pos.signum > 0))
+    runTest("zero.signum == 0")(assert(zero.signum === 0))
+  }
+
+  runWith[Int](-3, 3, 0)
+  runWith[Long](-3, 3, 0)
+  runWith[Float](-3, 3, 0)
+  runWith[Double](-3, 3, 0)
+  runWith[BigInt](-3, 3, 0)
+  runWith[BigDecimal](-3, 3, 0)
+  runWith[Rational](-3, 3, 0)
+  runWith[Real](-3, 3, 0)
+  runWith[Complex[Double]](-3, 3, 0)
+}
+
+
+
