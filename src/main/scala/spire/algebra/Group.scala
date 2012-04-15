c0d5163ebc51d6925439ce875c13df6ac691c894
Tom Switzer <thomas.switzer@gmail.com>
2012-04-15 14:23:06 -0400

    Make AdditiveGroup extend Group like it should.

--- src/main/scala/spire/algebra/Group.scala
+++ src/main/scala/spire/algebra/Group.scala
@@ -9,7 +9,7 @@ object Group {
   def apply[G](implicit G: Group[G]): Group[G] = G
 }
 
-class AdditiveGroup[A:Ring] extends Monoid[A] {
+class AdditiveGroup[A:Ring] extends Group[A] {
   private val ring = implicitly[Ring[A]]
   def identity = ring.zero
   def inverse(a: A): A = ring.negate(a)
