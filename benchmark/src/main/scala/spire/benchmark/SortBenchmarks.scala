bc41f0ed72c9ad87792f5ee2c8f20b4c1c1e1865
Erik Osheim <d_m@plastic-idolatry.com>
2012-10-10 00:12:24 -0400

    Fixed some bugs, improved benchmark, improved code

---
+++ benchmark/src/main/scala/spire/benchmark/SortBenchmarks.scala
@@ -0,0 +1,106 @@
+package spire.benchmark
+
+import scala.{specialized => spec}
+import scala.annotation.tailrec
+
+import scala.util.Random
+import Random._
+
+import spire.algebra._
+import spire.math._
+import fpf._
+
+import spire.math.{Numeric => SpireN}
+import scala.math.{Numeric => ScalaN}
+
+import com.google.caliper.Runner 
+import com.google.caliper.SimpleBenchmark
+import com.google.caliper.Param
+
+object SortBenchmarks extends MyRunner {
+  val cls = classOf[SortBenchmarks]
+}
+
+class SortBenchmarks extends MyBenchmark with BenchmarkData {
+  //@Param(Array("6", "8", "11", "14", "17", "20"))
+  @Param(Array("3", "4", "6", "9", "13"))
+  var pow: Int = 0
+
+  //@Param(Array("int", "long", "float", "double"))
+  @Param(Array("int"))
+  var typ: String = null
+
+  var is: Array[Int] = null
+  var js: Array[Long] = null
+  var fs: Array[Float] = null
+  var ds: Array[Double] = null
+
+  override protected def setUp() {
+    val size = fun.pow(2, pow).toInt
+
+    is = if (typ == "int") init(size)(nextInt) else null
+    js = if (typ == "long") init(size)(nextLong) else null
+    fs = if (typ == "float") init(size)(nextFloat) else null
+    ds = if (typ == "double") init(size)(nextDouble) else null
+  }
+
+  def timeJavaSort(reps:Int) = run(reps) {
+    if (typ == "int") {
+      val arr = is.clone; java.util.Arrays.sort(arr); arr.length
+    } else if (typ == "long") {
+      val arr = js.clone; java.util.Arrays.sort(arr); arr.length
+    } else if (typ == "float") {
+      val arr = fs.clone; java.util.Arrays.sort(arr); arr.length
+    } else if (typ == "double") {
+      val arr = ds.clone; java.util.Arrays.sort(arr); arr.length
+    }
+  }
+
+  def timeScalaQuicksort(reps:Int) = run(reps) {
+    if (typ == "int") {
+      val arr = is.clone; scala.util.Sorting.quickSort(arr); arr.length
+    } else if (typ == "long") {
+      val arr = js.clone; scala.util.Sorting.quickSort(arr); arr.length
+    } else if (typ == "float") {
+      val arr = fs.clone; scala.util.Sorting.quickSort(arr); arr.length
+    } else if (typ == "double") {
+      val arr = ds.clone; scala.util.Sorting.quickSort(arr); arr.length
+    }
+  }
+
+  def timeSpireInsertionSort(reps:Int) = run(reps) {
+    if (typ == "int") {
+      val arr = is.clone; spire.math.Sorting.insertionSort(arr); arr.length
+    } else if (typ == "long") {
+      val arr = js.clone; spire.math.Sorting.insertionSort(arr); arr.length
+    } else if (typ == "float") {
+      val arr = fs.clone; spire.math.Sorting.insertionSort(arr); arr.length
+    } else if (typ == "double") {
+      val arr = ds.clone; spire.math.Sorting.insertionSort(arr); arr.length
+    }
+  }
+
+  def timeSpireMergeSort(reps:Int) = run(reps) {
+    if (typ == "int") {
+      val arr = is.clone; spire.math.Sorting.mergeSort(arr); arr.length
+    } else if (typ == "long") {
+      val arr = js.clone; spire.math.Sorting.mergeSort(arr); arr.length
+    } else if (typ == "float") {
+      val arr = fs.clone; spire.math.Sorting.mergeSort(arr); arr.length
+    } else if (typ == "double") {
+      val arr = ds.clone; spire.math.Sorting.mergeSort(arr); arr.length
+    }
+  }
+
+  def timeSpireQuickSort(reps:Int) = run(reps) {
+    if (typ == "int") {
+      val arr = is.clone; spire.math.Sorting.quickSort(arr); arr.length
+    } else if (typ == "long") {
+      val arr = js.clone; spire.math.Sorting.quickSort(arr); arr.length
+    } else if (typ == "float") {
+      val arr = fs.clone; spire.math.Sorting.quickSort(arr); arr.length
+    } else if (typ == "double") {
+      val arr = ds.clone; spire.math.Sorting.quickSort(arr); arr.length
+    }
+  }
+}
--- src/main/scala/spire/math/Sorting.scala
+++ src/main/scala/spire/math/Sorting.scala
@@ -2,6 +2,7 @@ package spire.math
 
 import scala.reflect.ClassTag
 import scala.{specialized => spec}
+import scala.annotation.tailrec
 import scala.math.min
 
 //import Implicits._
@@ -11,6 +12,25 @@ import scala.math.min
  */
 trait Sort {
   def sort[@spec A:Order:ClassTag](data:Array[A]): Unit
+}
+
+object InsertionSort extends Sort {
+  final def sort[@spec A:Order:ClassTag](data:Array[A]) = sort(data, 0, data.length)
+
+  final def sort[@spec A](data:Array[A], start:Int, end:Int)
+  (implicit o:Order[A], ct:ClassTag[A]) = {
+    var i = start + 1
+    while (i < end) {
+      val item = data(i)
+      var hole = i
+      while (hole > start && o.gt(data(hole - 1), item)) {
+        data(hole) = data(hole - 1)
+        hole -= 1
+      }
+      data(hole) = item
+      i += 1
+    }
+  }
 }
 
 /**
@@ -28,25 +48,32 @@ object MergeSort extends Sort {
     var buf2:Array[A] = Array.ofDim[A](len)
     var tmp:Array[A] = null
 
+    //var width = 1
+    //var step = 2
     var i = 0
-
-    var width = 1
-    var step = 2
+    var limit = len - 16
+    while (i < limit) { InsertionSort.sort(data, i, i + 16); i += 16 }
+    if (i < len) InsertionSort.sort(data, i, len)
+    var width = 16
+    var step = 32
     while (width < len) {
       i = 0
+      limit = len - step
+      while (i < limit) {
+        merge(buf1, buf2, i, i + width, i + step); i += step
+      }
       while (i < len) {
-        merge(buf1, buf2, i, min(i + width, len), min(i + step, len))
-        i += step
+        merge(buf1, buf2, i, min(i + width, len), len); i += step
       }
       tmp = buf2
       buf2 = buf1
       buf1 = tmp
-
+    
       width *= 2
       step *= 2
     }
 
-    buf1
+    if (buf1 != data) System.arraycopy(buf1, 0, data, 0, len)
   }
 
   // TODO: making this private breaks specialization, but we'd like to hide it
@@ -56,11 +83,11 @@ object MergeSort extends Sort {
    * sections of the input array. The start, mid and end parameters denote the
    * left and right ranges of the input to merge, as well as the area of the
    * ouput to write to.
-   *
-   * This method will be called approximately N times (where N is the length
-   * of the array to be sorted), which is why we inline it.
    */
-  @inline final def merge[@spec A](in:Array[A], out:Array[A], start:Int, mid:Int, end:Int)(implicit o:Order[A]) {
+  @inline final def merge[@spec A]
+    (in:Array[A], out:Array[A], start:Int, mid:Int, end:Int)
+    (implicit o:Order[A]) {
+          
     var ii = start
     var jj = mid
     var kk = start
@@ -75,114 +102,119 @@ object MergeSort extends Sort {
   }
 }
 
-///**
-// * This is a specialized version of Scala's built-in quicksort
-// * implementation. It is not a stable sort, and mutates its input. It does
-// * not allocate extra space.
-// */
-//object QuickSort {
-//  final def sort[@spec K](x:Array[K])(implicit o:Order[K], m:ClassTag[K]) {
-//    def swap(k:K, a: Int, b: Int) {
-//      val t = x(a)
-//      x(a) = x(b)
-//      x(b) = t
-//    }
-//    def vecswap(k:K, _a: Int, _b: Int, n: Int) {
-//      var a = _a
-//      var b = _b
-//      var i = 0
-//      while (i < n) {
-//        swap(k, a, b)
-//        i += 1
-//        a += 1
-//        b += 1
-//      }
-//    }
-//    def med3(k:K, a: Int, b: Int, c: Int) = {
-//      if (o.lt(x(a), x(b))) {
-//        if (o.lt(x(b), x(c))) b else if (o.lt(x(a), x(c))) c else a
-//      } else {
-//        if (o.lt(x(b), x(c))) b else if (o.lt(x(a), x(c))) c else a
-//      }
-//    }
-//    def sort2(k:K, off: Int, len: Int) {
-//      // Insertion sort on smallest arrays
-//      if (len < 7) {
-//        var i = off
-//        while (i < len + off) {
-//          var j = i
-//          while (j > off && o.gt(x(j-1), x(j))) {
-//            swap(k, j, j-1)
-//            j -= 1
-//          }
-//          i += 1
-//        }
-//      } else {
-//        // Choose a partition element, v
-//        var m = off + (len >> 1)        // Small arrays, middle element
-//        if (len > 7) {
-//          var l = off
-//          var n = off + len - 1
-//          if (len > 40) {        // Big arrays, pseudomedian of 9
-//            val s = len / 8
-//            l = med3(k, l, l+s, l+2*s)
-//            m = med3(k, m-s, m, m+s)
-//            n = med3(k, n-2*s, n-s, n)
-//          }
-//          m = med3(k, l, m, n) // Mid-size, med of 3
-//        }
-//        val v = x(m)
-//  
-//        // Establish Invariant: v* (<v)* (>v)* v*
-//        var a = off
-//        var b = a
-//        var c = off + len - 1
-//        var d = c
-//        var done = false
-//        while (!done) {
-//          while (b <= c && o.lteqv(x(b), v)) {
-//            if (o.eqv(x(b), v)) {
-//              swap(k, a, b)
-//              a += 1
-//            }
-//            b += 1
-//          }
-//          while (c >= b && o.gteqv(x(c), v)) {
-//            if (o.eqv(x(c), v)) {
-//              swap(k, c, d)
-//              d -= 1
-//            }
-//            c -= 1
-//          }
-//          if (b > c) {
-//            done = true
-//          } else {
-//            swap(k, b, c)
-//            c -= 1
-//            b += 1
-//          }
-//        }
-//  
-//        // Swap partition elements back to middle
-//        val n = off + len
-//        var s = math.min(a-off, b-a)
-//        vecswap(k, off, b-s, s)
-//        s = math.min(d-c, n-d-1)
-//        vecswap(k, b, n-s, s)
-//  
-//        // Recursively sort non-partition-elements
-//        s = b - a
-//        if (s > 1)
-//          sort2(k, off, s)
-//        s = d - c
-//        if (s > 1)
-//          sort2(k, n-s, s)
-//      }
-//    }
-//
-//    sort2(x(0), 0, x.length)
-//  }
-//}
+/**
+ * This is a specialized version of Scala's built-in quicksort
+ * implementation. It is not a stable sort, and mutates its input. It does
+ * not allocate extra space.
+ */
+object QuickSort {
+  final def sort[@spec K](x:Array[K])(implicit o:Order[K], ct:ClassTag[K]) {
+    sort2(x:Array[K], x(0), 0, x.length)
+  }
+
+  def sort2[@spec K](x: Array[K], k:K, off: Int, len: Int)
+    (implicit o:Order[K], ct:ClassTag[K]) {
+
+    def swap(k:K, a: Int, b: Int) {
+      val t = x(a)
+      x(a) = x(b)
+      x(b) = t
+    }
+  
+    def vecswap(k:K, _a: Int, _b: Int, n: Int) {
+      var a = _a
+      var b = _b
+      var i = 0
+      while (i < n) {
+        swap(k, a, b)
+        i += 1
+        a += 1
+        b += 1
+      }
+    }
+  
+    def med3(k:K, a: Int, b: Int, c: Int) = {
+      if (o.lt(x(a), x(b))) {
+        if (o.lt(x(b), x(c))) b else if (o.lt(x(a), x(c))) c else a
+      } else {
+        if (o.lt(x(b), x(c))) b else if (o.lt(x(a), x(c))) c else a
+      }
+    }
+
+    // Insertion sort on smallest arrays
+    if (len < 7) {
+      var i = off
+      while (i < len + off) {
+        var j = i
+        while (j > off && o.gt(x(j-1), x(j))) {
+          swap(k, j, j-1)
+          j -= 1
+        }
+        i += 1
+      }
+    } else {
+      // Choose a partition element, v
+      var m = off + (len >> 1)        // Small arrays, middle element
+      if (len > 7) {
+        var l = off
+        var n = off + len - 1
+        if (len > 40) {        // Big arrays, pseudomedian of 9
+          val s = len / 8
+          l = med3(k, l, l+s, l+2*s)
+          m = med3(k, m-s, m, m+s)
+          n = med3(k, n-2*s, n-s, n)
+        }
+        m = med3(k, l, m, n) // Mid-size, med of 3
+      }
+      val v = x(m)
+
+      // Establish Invariant: v* (<v)* (>v)* v*
+      var a = off
+      var b = a
+      var c = off + len - 1
+      var d = c
+      var done = false
+      while (!done) {
+        while (b <= c && o.lteqv(x(b), v)) {
+          if (o.eqv(x(b), v)) {
+            swap(k, a, b)
+            a += 1
+          }
+          b += 1
+        }
+        while (c >= b && o.gteqv(x(c), v)) {
+          if (o.eqv(x(c), v)) {
+            swap(k, c, d)
+            d -= 1
+          }
+          c -= 1
+        }
+        if (b > c) {
+          done = true
+        } else {
+          swap(k, b, c)
+          c -= 1
+          b += 1
+        }
+      }
+
+      // Swap partition elements back to middle
+      val n = off + len
+      var s = math.min(a-off, b-a)
+      vecswap(k, off, b-s, s)
+      s = math.min(d-c, n-d-1)
+      vecswap(k, b, n-s, s)
+
+      // Recursively sort non-partition-elements
+      s = b - a
+      if (s > 1)
+        sort2(x, k, off, s)(o, ct)
+      s = d - c
+      if (s > 1)
+        sort2(x, k, n-s, s)(o, ct)
+    }
+  }
+}
 
 // TODO: would be nice to try implementing e.g. Tim Peters' sort algorithm.
 
@@ -191,6 +223,7 @@ object MergeSort extends Sort {
  * uses extra memory. Quicksort is a bit faster but is O(n^2) in worst case.
  */
 object Sorting {
+  final def insertionSort[@spec A:Order:ClassTag](data:Array[A]) = InsertionSort.sort(data)
   final def mergeSort[@spec A:Order:ClassTag](data:Array[A]) = MergeSort.sort(data)
-  //final def quickSort[@spec K:Order:ClassTag](data:Array[K]) = QuickSort.sort(data)
+  final def quickSort[@spec K:Order:ClassTag](data:Array[K]) = QuickSort.sort(data)
 }
