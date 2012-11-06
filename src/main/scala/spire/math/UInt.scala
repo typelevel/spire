69ec300e5219eeb5caceca707e84ee4234c8c5ce
Erik Osheim <d_m@plastic-idolatry.com>
2012-11-06 10:48:33 -0500

    first rough cut of UInt/ULong

---
+++ src/main/scala/spire/math/UInt.scala
@@ -0,0 +1,58 @@
+package spire.math
+
+import language.implicitConversions
+
+import java.lang.Math.pow
+
+object UInt {
+  @inline implicit final def apply(n: Int) = new UInt(n)
+  @inline implicit final def apply(n: Long) = new UInt(n.toInt)
+}
+
+class UInt(val signed: Int) extends AnyVal {
+  def toByte: Byte = signed.toByte
+  def toChar: Char = signed.toChar
+  def toShort: Short = signed.toShort
+  def toInt: Int = signed
+  def toLong: Long = signed & 0xffffffffL
+  def toFloat: Float = toLong.toFloat
+  def toDouble: Double = toLong.toDouble
+
+  def isValidByte = toInt == toByte
+  def isValidShort = toInt == toShort
+  def isValidChar = toInt == toChar
+  def isValidInt = signed >= 0
+  def isValidLong = true
+
+  override def toString: String = toLong.toString
+  
+  def == (that: Long): Boolean = this.signed == that.toInt
+  def == (that: UInt): Boolean = this.signed == that.signed
+
+  def != (that: Long): Boolean = this.signed != that.toInt
+  def != (that: UInt): Boolean = this.signed != that.signed
+
+  def <= (that: UInt) = this.toLong <= that.toLong
+  def < (that: UInt) = this.toLong < that.toLong
+  def >= (that: UInt) = this.toLong >= that.toLong
+  def > (that: UInt) = this.toLong > that.toLong
+  
+  def unary_- (that: UInt) = new UInt(this.signed)
+
+  def + (that: UInt) = new UInt(this.signed + that.signed)
+  def - (that: UInt) = new UInt(this.signed - that.signed)
+  def * (that: UInt) = new UInt(this.signed * that.signed)
+  def / (that: UInt) = UInt(this.toLong / that.toLong)
+  def % (that: UInt) = UInt(this.toLong % that.toLong)
+
+  def unary_~ (that: UInt) = new UInt(this.signed)
+
+  def << (shift: UInt) = new UInt(this.signed << shift.signed)
+  def >> (shift: UInt) = new UInt(this.signed >> shift.signed)
+  def >>> (shift: UInt) = new UInt(this.signed >>> shift.signed)
+  def & (that: UInt) = new UInt(this.signed & that.signed)
+  def | (that: UInt) = new UInt(this.signed | that.signed)
+  def ^ (that: UInt) = new UInt(this.signed ^ that.signed)
+
+  def ** (that: UInt) = new UInt(pow(this.toDouble, that.toDouble).toInt)
+}
---
+++ src/main/scala/spire/math/ULong.scala
@@ -0,0 +1,104 @@
+package spire.math
+
+import language.implicitConversions
+
+import java.lang.Math.pow
+import scala.annotation.tailrec
+
+object ULong {
+  @inline final def apply(n: Long): ULong = new ULong(n)
+
+  implicit final def fromInt(n: Int): ULong = new ULong(n & 0xffffffffL)
+  implicit final def fromLong(n: Long): ULong = new ULong(n)
+  implicit final def toLong(u: ULong): Long = u.signed
+
+
+  @tailrec final private[math] def pow(t:Long, b:Long, e:Long): ULong = {
+    if (e == 0L) new ULong(t)
+    else if ((e & 1L) == 1L) pow(t * b, b * b, e >>> 1L)
+    else pow(t, b * b, e >>> 1L)
+  }
+
+  @tailrec final private[math] def gcd(a:ULong, b:ULong): ULong = {
+    if (b == new ULong(0L)) a else gcd(b, a % b)
+  }
+}
+
+class ULong(val signed: Long) extends AnyVal {
+  final def toByte: Byte = signed.toByte
+  final def toChar: Char = signed.toChar
+  final def toShort: Short = signed.toShort
+  final def toInt: Int = signed.toInt
+  final def toLong: Long = signed
+
+  final def toFloat: Float = if (signed < 0)
+    -(Long.MinValue.toFloat) - signed.toFloat
+  else
+    signed.toFloat
+
+  final def toDouble: Double = if (signed < 0)
+    -(Long.MinValue.toDouble) - signed.toDouble
+  else
+    signed.toDouble
+
+  override final def toString: String = if (this.signed >= 0L)
+    this.signed.toString
+  else
+    (-BigInt(Long.MinValue) * 2 + BigInt(this.signed)).toString // ugh, fixme
+  
+  final def == (that: ULong): Boolean = this.signed == that.signed
+  final def != (that: ULong): Boolean = this.signed != that.signed
+
+  final def <= (that: ULong) = if (this.signed >= 0L)
+    this.signed <= that.signed || that.signed < 0L
+  else
+    that.signed <= this.signed
+
+  final def < (that: ULong) = if (this.signed >= 0L)
+    this.signed < that.signed || that.signed < 0L
+  else
+    that.signed < this.signed
+
+  @inline final def >= (that: ULong) = that <= this
+  @inline final def > (that: ULong) = that < this
+  
+  final def unary_- (that: ULong) = new ULong(this.signed)
+
+  final def + (that: ULong) = new ULong(this.signed + that.signed)
+  final def - (that: ULong) = new ULong(this.signed - that.signed)
+  final def * (that: ULong) = new ULong(this.signed * that.signed)
+
+  final def / (that: ULong) = {
+    val n: Long = this.signed
+    val d: Long = that.signed
+
+    if (d == 0) {
+      throw new java.lang.ArithmeticException("/ by zero")
+    } else if (d < 0) {
+      new ULong(if (n >= 0 || n < d) 0 else 1)
+    } else {
+      val half = n >>> 1
+      new ULong(((half / d) << 1) + (((half % d) << 1) + (n & 1)) / d)
+    }
+  }
+
+  final def % (that: ULong) = this - (this / that) * that
+
+  final def /% (that: ULong) = {
+    val q = this / that
+    (q, this - q * that)
+  }
+
+  final def unary_~ (that: ULong) = new ULong(this.signed)
+
+  final def << (shift: ULong) = new ULong(this.signed << shift.signed)
+  final def >> (shift: ULong) = new ULong(this.signed >> shift.signed)
+  final def >>> (shift: ULong) = new ULong(this.signed >>> shift.signed)
+  final def & (that: ULong) = new ULong(this.signed & that.signed)
+  final def | (that: ULong) = new ULong(this.signed | that.signed)
+  final def ^ (that: ULong) = new ULong(this.signed ^ that.signed)
+
+  final def ** (that: ULong) = ULong.pow(1L, this.signed, that.signed)
+
+  final def gcd (that: ULong) = ULong.gcd(this, that)
+}
