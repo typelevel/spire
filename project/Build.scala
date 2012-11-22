96649e45256059fcaa865288e9c76331d7cac447
Erik Osheim <d_m@plastic-idolatry.com>
2012-11-22 14:24:22 -0500

    version bump to M5 and 2.10.0-RC3

--- project/Build.scala
+++ project/Build.scala
@@ -6,18 +6,18 @@ object MyBuild extends Build {
   override lazy val settings = super.settings ++ Seq(
     name := "spire",
     organization := "org.spire-math",
-    version := "0.3.0-M4",
+    version := "0.3.0-M5",
 
-    scalaVersion := "2.10.0-RC2",
-    scalaBinaryVersion := "2.10.0-RC2",
+    scalaVersion := "2.10.0-RC3",
+    scalaBinaryVersion := "2.10.0-RC3",
 
     licenses := Seq("BSD-style" -> url("http://opensource.org/licenses/MIT")),
     homepage := Some(url("http://spire-math.org")),
 
     libraryDependencies ++= Seq(
-      "org.scalatest" %% "scalatest" % "1.8" % "test",
+      "org.scalatest" % "scalatest_2.10.0-RC3" % "1.8-B1" % "test",
       "org.scalacheck" %% "scalacheck" % "1.10.0" % "test",
-      "org.scala-lang" % "scala-reflect" % "2.10.0-RC2"
+      "org.scala-lang" % "scala-reflect" % "2.10.0-RC3"
     ),
 
     scalacOptions ++= Seq(
