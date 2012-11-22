34016895675438da425b9b96a5aa1d81982d246a
Erik Osheim <d_m@plastic-idolatry.com>
2012-11-22 14:24:02 -0500

    added some methods to BooleanAlgebra

--- src/main/scala/spire/algebra/BooleanAlgebra.scala
+++ src/main/scala/spire/algebra/BooleanAlgebra.scala
@@ -9,17 +9,22 @@ import spire.macrosk.Ops
 /**
  * A boolean algebra is a structure that defines a few basic operations, namely
  * as conjunction (&), disjunction (|), and negation (~). Both conjunction and
- * disjunction must be associative, commutative and should distrubte over each
- * other. Also, both have an identity. Also, they obey the absortion laws, that
+ * disjunction must be associative, commutative and should distribute over each
+ * other. Also, both have an identity. Also, they obey the absorption law, that
  * is `x & (y | x) == x` and `x | (x & y) == x`.
  */
 trait BooleanAlgebra[@spec(Boolean, Byte, Short, Int, Long) A] { self =>
   def one: A
   def zero: A
+  def complement(a: A): A
   def and(a: A, b: A): A
   def or(a: A, b: A): A
-  def complement(a: A): A
   def xor(a: A, b: A): A = or(and(a, complement(b)), and(complement(a), b))
+
+  def imp(a: A, b: A): A = or(complement(a), b)
+  def nand(a: A, b: A): A = complement(and(a, b))
+  def nor(a: A, b: A): A = complement(or(a, b))
+  def nxor(a: A, b: A): A = and(or(a, complement(b)), or(complement(a), b))
 
   def dual: BooleanAlgebra[A] = new BooleanAlgebra[A] {
     def one: A = self.zero
@@ -28,6 +33,8 @@ trait BooleanAlgebra[@spec(Boolean, Byte, Short, Int, Long) A] { self =>
     def or(a: A, b: A): A = self.and(a, b)
     def complement(a: A): A = self.complement(a)
     override def xor(a: A, b: A): A = self.complement(self.xor(a, b))
+
+    override def dual: BooleanAlgebra[A] = self
   }
 }
 
@@ -133,5 +140,3 @@ trait ULongIsBooleanAlgebra extends BooleanAlgebra[ULong] {
   def complement(a: ULong): ULong = ~a
   override def xor(a: ULong, b: ULong): ULong = a ^ b
 }
-
-
