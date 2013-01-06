c4dc3ddcbeaff6d286936150bf9387c66e0a54ad
Erik Osheim <d_m@plastic-idolatry.com>
2013-01-05 19:27:54 -0500

    Fix spire.math.compat implicit precedence.

--- src/main/scala/spire/math/compat.scala
+++ src/main/scala/spire/math/compat.scala
@@ -2,38 +2,44 @@ package spire.math
 
 import spire.algebra._
 
-object compat {
-  implicit def equiv[A: Eq] =
-    new ScalaEquivWrapper[A] {
-      val eq = Eq[A]
-    }
-
-  implicit def ordering[A: Order] =
-    new ScalaOrderingWrapper[A] {
-      val order = Order[A]
-    }
-
-  implicit def fractional [A: Field: ConvertableFrom: Signed: Order] =
-    new ScalaFractionalWrapper[A] {
-      val order = Order[A]
-      val structure = Field[A]
-      val conversions = ConvertableFrom[A]
-      val signed = Signed[A]
-    }
-
-  implicit def integral[A: EuclideanRing: ConvertableFrom: Signed: Order] =
-    new ScalaIntegralWrapper[A] {
-      val order = Order[A]
-      val structure = EuclideanRing[A]
-      val conversions = ConvertableFrom[A]
-      val signed = Signed[A]
-    }
-
-  implicit def numeric[A: Ring: ConvertableFrom: Signed: Order] =
+private[math] trait LowViz1 {
+  implicit def numeric[A: Ring: ConvertableFrom: Signed: Order]: scala.math.Numeric[A] =
     new ScalaNumericWrapper[A] {
       val order = Order[A]
       val structure = Ring[A]
       val conversions = ConvertableFrom[A]
       val signed = Signed[A]
     }
+
+  implicit def ordering[A: Order]: scala.math.Ordering[A] =
+    new ScalaOrderingWrapper[A] {
+      val order = Order[A]
+    }
+
+  implicit def equiv[A: Eq]: scala.math.Equiv[A] =
+    new ScalaEquivWrapper[A] {
+      val eq = Eq[A]
+    }
 }
+
+private[math] trait LowViz2 extends LowViz1 {
+  implicit def fractional [A: Field: ConvertableFrom: Signed: Order]: scala.math.Fractional[A] =
+    new ScalaFractionalWrapper[A] {
+      val order = Order[A]
+      val structure = Field[A]
+      val conversions = ConvertableFrom[A]
+      val signed = Signed[A]
+    }
+}
+
+private[math] trait LowViz3 extends LowViz2 {
+  implicit def integral[A: EuclideanRing: ConvertableFrom: Signed: Order]: scala.math.Integral[A] =
+    new ScalaIntegralWrapper[A] {
+      val order = Order[A]
+      val structure = EuclideanRing[A]
+      val conversions = ConvertableFrom[A]
+      val signed = Signed[A]
+    }
+}
+
+object compat extends LowViz3
