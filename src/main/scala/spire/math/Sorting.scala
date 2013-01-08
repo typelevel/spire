96d32aaf0637d6114ded8402711468fc4ba3168e
Erik Osheim <d_m@plastic-idolatry.com>
2013-01-08 16:24:44 -0500

    Revert "Handle NaN more gracefully."
    This reverts commit ae016ffae99986e5f6c227dba1b020f9bab03458.
    
    The performance impact is relatively minor but real. I think a better
    way to go will be to have another instance of Order[Double] which
    provides a total ordering including NaN.

--- src/main/scala/spire/math/Sorting.scala
+++ src/main/scala/spire/math/Sorting.scala
@@ -28,8 +28,7 @@ object InsertionSort extends Sort {
     while (i < end) {
       val item = data(i)
       var hole = i
-      //while (hole > start && o.gt(data(hole - 1), item)) {
-      while (hole > start && o.compare(data(hole - 1), item) > 0) {
+      while (hole > start && o.gt(data(hole - 1), item)) {
         data(hole) = data(hole - 1)
         hole -= 1
       }
@@ -98,8 +97,7 @@ object MergeSort extends Sort {
     var jj = mid
     var kk = start
     while (kk < end) {
-      //if (ii < mid && (jj >= end || o.lteqv(in(ii), in(jj)))) {
-      if (ii < mid && (jj >= end || o.compare(in(ii), in(jj)) < 1)) {
+      if (ii < mid && (jj >= end || o.lteqv(in(ii), in(jj)))) {
         out(kk) = in(ii); ii += 1
       } else {
         out(kk) = in(jj); jj += 1
@@ -143,8 +141,7 @@ object QuickSort {
     var store = left
     var i = left
     while (i < right) {
-      //if (o.lt(data(i), value)) {
-      if (o.compare(data(i), value) < 0) {
+      if (o.lt(data(i), value)) {
         //swap(i, store)
         tmp = data(i); data(i) = data(store); data(store) = tmp
         store += 1
