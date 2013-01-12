d88fcad4b6f1b0205db494a5a550348e946b9dbb
Erik Osheim <d_m@plastic-idolatry.com>
2013-01-12 15:47:11 -0500

    add Boolean to Rig

--- src/main/scala/spire/algebra/Rig.scala
+++ src/main/scala/spire/algebra/Rig.scala
@@ -25,6 +25,7 @@ trait Rig[@spec(Int,Long,Float,Double) A] extends Semiring[A] with AdditiveMonoi
 }
 
 object Rig {
+  implicit object BooleanIsRig extends BooleanIsRig
   implicit object UByteIsRig extends UByteIsRig
   implicit object UShortIsRig extends UShortIsRig
   implicit object UIntIsRig extends UIntIsRig
@@ -34,6 +35,14 @@ object Rig {
   implicit def ringIsRig[@spec(Int,Long,Float,Double) A: Ring]: Rig[A] = Ring[A]
 
   @inline final def apply[A](implicit r:Rig[A]):Rig[A] = r
+}
+
+trait BooleanIsRig extends Rig[Boolean] {
+  def one: Boolean = true
+  def plus(a:Boolean, b:Boolean): Boolean = a || b
+  override def pow(a:Boolean, b:Int): Boolean = a
+  override def times(a:Boolean, b:Boolean): Boolean = a && b
+  def zero: Boolean = false
 }
 
 trait UByteIsRig extends Rig[UByte] {
