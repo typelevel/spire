b32aff711c639589bc27a930938c802103338320
Tom Switzer <thomas.switzer@gmail.com>
2013-01-12 02:35:52 -0500

    Add more collection monoids.

--- src/main/scala/spire/algebra/Monoid.scala
+++ src/main/scala/spire/algebra/Monoid.scala
@@ -17,10 +17,13 @@ object Monoid extends Monoid0 {
   implicit object StringMonoid extends StringMonoid
   implicit def SetMonoid[A]: Monoid[Set[A]] = new CollMonoid[A, Set[A]]
   implicit def ListMonoid[A]: Monoid[List[A]] = new CollMonoid[A, List[A]]
+  implicit def VectorMonoid[A]: Monoid[Vector[A]] = new CollMonoid[A, Vector[A]]
 }
 
 trait Monoid0 {
   implicit def group[A: Group]: Monoid[A] = Group[A]
+
+  implicit def SeqMonoid[A]: Monoid[Seq[A]] = new CollMonoid[A, Seq[A]]
 }
 
 trait StringMonoid extends Monoid[String] {
@@ -32,3 +35,4 @@ final class CollMonoid[A, SA <: TraversableLike[A, SA]](implicit cbf: CanBuildFr
   def id: SA = cbf().result()
   def op(x: SA, y: SA): SA = x.++(y)(cbf)
 }
+
