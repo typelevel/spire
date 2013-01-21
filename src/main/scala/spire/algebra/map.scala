0334288cfa78d9de8b4821cb3d91a97acc858f1a
Erik Osheim <d_m@plastic-idolatry.com>
2013-01-21 01:20:53 -0500

    Refactor the map type class instances.
    This removes some inlining warnings (by manually inlining the code) and
    also moves conditionals outside of the "inner loops" when possible.

--- src/main/scala/spire/algebra/map.scala
+++ src/main/scala/spire/algebra/map.scala
@@ -2,28 +2,18 @@ package spire.algebra
 
 import scala.{ specialized => spec }
 
-private object MapSupport {
-  @inline final def opf[A](x: A, y: A, flip: Boolean)(implicit A: Semigroup[A]): A = {
-    if (flip) A.op(y, x) else A.op(x, y)
-  }
-}
-
-import MapSupport._
-
 trait MapMonoid[K, V] extends Monoid[Map[K, V]] {
   implicit def scalar: Semigroup[V]
 
   def id: Map[K, V] = Map.empty
 
-  def op(x: Map[K, V], y: Map[K, V]): Map[K, V] =
-    if (x.size < y.size) plus0(y, x, true) else plus0(x, y, false)
-
-  @inline private final def plus0(x: Map[K, V], y: Map[K, V], flip: Boolean): Map[K, V] = {
-    y.foldLeft(x) { (z, kv) =>
-      z + ((kv._1, (x get kv._1) match {
-        case Some(u) => opf(u, kv._2, flip)
-        case None => kv._2
-      }))
+  def op(x: Map[K, V], y: Map[K, V]): Map[K, V] = {
+    var xx = x
+    var yy = y
+    var f = scalar.op _
+    if (x.size < y.size) { xx = y; yy = x; f = (x: V, y: V) => scalar.op(y, x) }
+    yy.foldLeft(xx) { (z, kv) =>
+      z.updated(kv._1, (xx get kv._1).map(u => f(u, kv._2)).getOrElse(kv._2))
     }
   }
 }
@@ -31,55 +21,44 @@ trait MapMonoid[K, V] extends Monoid[Map[K, V]] {
 trait MapGroup[K, V] extends MapMonoid[K, V] with Group[Map[K, V]] {
   implicit def scalar: Group[V]
 
-  def inverse(x: Map[K, V]): Map[K, V] = x mapValues (scalar.inverse(_))
+  def inverse(x: Map[K, V]): Map[K, V] = x.mapValues(scalar.inverse)
 }
 
 trait MapRng[K, V] extends RingAlgebra[Map[K, V], V] { self =>
   def zero: Map[K, V] = Map.empty
 
-  def plus(x: Map[K, V], y: Map[K, V]): Map[K, V] =
-    if (x.size < y.size) plus0(y, x) else plus0(x, y)
+  def plus(x: Map[K, V], y: Map[K, V]): Map[K, V] = {
+    var xx = x
+    var yy = y
+    if (x.size < y.size) { xx = y; yy = x }
+    yy.foldLeft(xx) { (z, kv) =>
+      z.updated(kv._1, (xx get kv._1).map(u => scalar.plus(u, kv._2)).getOrElse(kv._2))
+    }
+  }
 
   def negate(x: Map[K, V]): Map[K, V] = x mapValues (scalar.negate(_))
 
-  def times(x: Map[K, V], y: Map[K, V]): Map[K, V] =
-    if (x.size < y.size) times0(y, x, true) else times0(x, y, false)
+  def times(x: Map[K, V], y: Map[K, V]): Map[K, V] = {
+    var xx = x
+    var yy = y
+    var f = scalar.times _
+    if (x.size < y.size) { xx = y; yy = x; f = (x: V, y: V) => scalar.times(y, x) }
+    yy.foldLeft(zero) { (z, kv) =>
+      (xx get kv._1).map(u => z.updated(kv._1, f(u, kv._2))).getOrElse(z)
+    }
+  }
   
   def timesl(r: V, v: Map[K, V]): Map[K, V] = v mapValues (scalar.times(r, _))
-
-  // Rng addition is commutative, so we can optimize a bit.
-  @inline private final def plus0(x: Map[K, V], y: Map[K, V]): Map[K, V] = {
-    y.foldLeft(x) { (z, kv) =>
-      z + ((kv._1, (x get kv._1) match {
-        case Some(u) => scalar.plus(u, kv._2)
-        case None => kv._2
-      }))
-    }
-  }
-
-  @inline private final def times0(x: Map[K, V], y: Map[K, V], flip: Boolean): Map[K, V] = {
-    y.foldLeft(zero) { (z, kv) =>
-      (x get kv._1) match {
-        case Some(u) =>
-          z + ((kv._1, if (flip) scalar.times(kv._2, u) else scalar.times(u, kv._2)))
-        case None =>
-          z
-      }
-    }
-  }
 }
 
 trait MapVectorSpace[K, V] extends MapRng[K, V] with VectorSpace[Map[K, V], V] {
-  override def times(x: Map[K, V], y: Map[K, V]): Map[K, V] =
-    if (x.size < y.size) times0(y, x, true) else times0(x, y, false)
-  
-  // Fields are commutative, so optimize a bit.
-  @inline private final def times0(x: Map[K, V], y: Map[K, V], flip: Boolean): Map[K, V] = {
-    y.foldLeft(zero) { (z, kv) =>
-      (x get kv._1) match {
-        case Some(u) => z + ((kv._1, scalar.times(u, kv._2)))
-        case None => z
-      }
+  override def times(x: Map[K, V], y: Map[K, V]): Map[K, V] = {
+    var xx = x
+    var yy = y
+    var f = scalar.times _
+    if (x.size < y.size) { xx = y; yy = x }
+    yy.foldLeft(zero) { (z, kv) =>
+      (xx get kv._1).map(u => z.updated(kv._1, scalar.times(u, kv._2))).getOrElse(z)
     }
   }
 }
