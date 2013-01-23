a70902804067c464491ee0091397d79a3b1ce0f1
Erik Osheim <d_m@plastic-idolatry.com>
2013-01-23 00:57:48 -0500

    Bump version to 0.3.0-RC2.
    In light of the awesome reorganization by Lars, as well as the
    backporting effort to 2.9 and bugfixes, it seems worth it to
    mark a new RC.

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ object MyBuild extends Build {
 
   override lazy val settings = super.settings ++ Seq(
     organization := "org.spire-math",
-    version := "0.3.0-RC1",
+    version := "0.3.0-RC2",
 
     scalaVersion := "2.10.0",
     //scalaBinaryVersion := "2.10.0",
