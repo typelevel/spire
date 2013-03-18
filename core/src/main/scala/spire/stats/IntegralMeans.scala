16fa23af243c43a4ae0013f39d083eeddb0f4fb6
Christian Krause <kizkizzbangbang@googlemail.com>
2013-03-18 08:00:50 +0100

    provides GenTraversableOnceIntegralMeans

--- core/src/main/scala/spire/stats/IntegralMeans.scala
+++ core/src/main/scala/spire/stats/IntegralMeans.scala
@@ -3,6 +3,7 @@ package spire.stats
 import language.higherKinds
 
 import scala.collection.GenTraversable
+import scala.collection.GenTraversableOnce
 
 import spire.math.Integral
 
@@ -17,6 +18,7 @@ trait IntegralMeans0 {
 
 object IntegralMeans extends IntegralMeans0 {
   implicit object ArrayMeans extends ArrayIntegralMeans
+  implicit object GenTraversableOnceMeans extends GenTraversableOnceIntegralMeans
 }
 
 trait ArrayIntegralMeans extends IntegralMeans[Array] {
@@ -40,5 +42,21 @@ trait GenTraversableIntegralMeans[CC[X] <: GenTraversable[X]] extends IntegralMe
   def arithmeticMeanBy[A,B](xs: CC[A])(f: A ⇒ B)(implicit num: Integral[B]): B = {
     val sum = xs.aggregate(num.zero)((b,a) ⇒ num.plus(b, f(a)), num.plus)
     num.quot(sum, num.fromInt(xs.size))
+  }
+}
+
+trait GenTraversableOnceIntegralMeans extends IntegralMeans[GenTraversableOnce] {
+  def arithmeticMean[A](xs: GenTraversableOnce[A])(implicit num: Integral[A]): A = {
+    val seqop = (b: (A,Int), a: A) ⇒ (num.plus(b._1,a), b._2 + 1)
+    val combop = (a: (A,Int), b: (A,Int)) ⇒ (num.plus(a._1,b._1), a._2 + b._2)
+    val (sum,size) = xs.aggregate((num.zero,0))(seqop, combop)
+    num.quot(sum, num.fromInt(size))
+  }
+
+  def arithmeticMeanBy[A,B](xs: GenTraversableOnce[A])(f: A ⇒ B)(implicit num: Integral[B]): B = {
+    val seqop = (b: (B,Int), a: A) ⇒ (num.plus(b._1,f(a)), b._2 + 1)
+    val combop = (a: (B,Int), b: (B,Int)) ⇒ (num.plus(a._1,b._1), a._2 + b._2)
+    val (sum,size) = xs.aggregate((num.zero,0))(seqop, combop)
+    num.quot(sum, num.fromInt(size))
   }
 }
