40525a1fdb740639f5a8a267da3ad1109aaf62d6
Tom Switzer <thomas.switzer@gmail.com>
2013-04-06 09:46:42 -0400

    Refactor Auto a bit to make way for some code del.

--- core/src/main/scala/spire/macros/Auto.scala
+++ core/src/main/scala/spire/macros/Auto.scala
@@ -17,67 +17,67 @@ object Auto {
   def order[A]() = macro orderImpl[A]
 
   def semiringImpl[A: c.WeakTypeTag](c: Context)(): c.Expr[Semiring[A]] = {
-    val ops = new AutoOps { val ctx = c }
+    val ops = new ScalaOps { val ctx = c }
     c.universe.reify {
       new Semiring[A] {
-        def plus(x: A, y: A): A = ops.plus.splice
-        def times(x: A, y: A): A = ops.times.splice
+        def plus(x: A, y: A): A = ops.plus[A].splice
+        def times(x: A, y: A): A = ops.times[A].splice
       }
     }
   }
 
   def rigImpl[A: c.WeakTypeTag](c: Context)(z: c.Expr[A], o: c.Expr[A]): c.Expr[Rig[A]] = {
-    val ops = new AutoOps { val ctx = c }
+    val ops = new ScalaOps { val ctx = c }
     c.universe.reify {
       new Rig[A] {
         def zero: A = z.splice
         def one: A = o.splice
-        def plus(x: A, y: A): A = ops.plus.splice
-        def times(x: A, y: A): A = ops.times.splice
+        def plus(x: A, y: A): A = ops.plus[A].splice
+        def times(x: A, y: A): A = ops.times[A].splice
       }
     }
   }
 
   def rngImpl[A: c.WeakTypeTag](c: Context)(z: c.Expr[A]): c.Expr[Rng[A]] = {
-    val ops = new AutoOps { val ctx = c }
+    val ops = new ScalaOps { val ctx = c }
     c.universe.reify {
       new Rng[A] {
         def zero: A = z.splice
-        def plus(x: A, y: A): A = ops.plus.splice
-        def times(x: A, y: A): A = ops.times.splice
-        override def minus(x: A, y: A): A = ops.minus.splice
-        def negate(x: A): A = ops.negate.splice
+        def plus(x: A, y: A): A = ops.plus[A].splice
+        def times(x: A, y: A): A = ops.times[A].splice
+        override def minus(x: A, y: A): A = ops.minus[A].splice
+        def negate(x: A): A = ops.negate[A].splice
       }
     }
   }
 
   def ringImpl[A: c.WeakTypeTag](c: Context)(z: c.Expr[A], o: c.Expr[A]): c.Expr[Ring[A]] = {
-    val ops = new AutoOps { val ctx = c }
+    val ops = new ScalaOps { val ctx = c }
     c.universe.reify {
       new Ring[A] {
         def zero: A = z.splice
         def one: A = o.splice
-        def plus(x: A, y: A): A = ops.plus.splice
-        def times(x: A, y: A): A = ops.times.splice
-        override def minus(x: A, y: A): A = ops.minus.splice
-        def negate(x: A): A = ops.negate.splice
+        def plus(x: A, y: A): A = ops.plus[A].splice
+        def times(x: A, y: A): A = ops.times[A].splice
+        override def minus(x: A, y: A): A = ops.minus[A].splice
+        def negate(x: A): A = ops.negate[A].splice
       }
     }
   }
 
   def euclideanRingImpl[A: c.WeakTypeTag](c: Context)
       (z: c.Expr[A], o: c.Expr[A])(ev: c.Expr[Eq[A]]): c.Expr[EuclideanRing[A]] = {
-    val ops = new AutoOps { val ctx = c }
+    val ops = new ScalaOps { val ctx = c }
     c.universe.reify {
       new EuclideanRing[A] {
         def zero: A = z.splice
         def one: A = o.splice
-        def plus(x: A, y: A): A = ops.plus.splice
-        def times(x: A, y: A): A = ops.times.splice
-        override def minus(x: A, y: A): A = ops.minus.splice
-        def negate(x: A): A = ops.negate.splice
-        def quot(x: A, y: A): A = ops.div.splice
-        def mod(x: A, y: A): A = ops.mod.splice
+        def plus(x: A, y: A): A = ops.plus[A].splice
+        def times(x: A, y: A): A = ops.times[A].splice
+        override def minus(x: A, y: A): A = ops.minus[A].splice
+        def negate(x: A): A = ops.negate[A].splice
+        def quot(x: A, y: A): A = ops.div[A].splice
+        def mod(x: A, y: A): A = ops.mod[A].splice
         def gcd(x: A, y: A): A = euclid(x, y)(ev.splice)
       }
     }
@@ -85,25 +85,25 @@ object Auto {
 
   def fieldImpl[A: c.WeakTypeTag](c: Context)
       (z: c.Expr[A], o: c.Expr[A])(ev: c.Expr[Eq[A]]): c.Expr[Field[A]] = {
-    val ops = new AutoOps { val ctx = c }
+    val ops = new ScalaOps { val ctx = c }
     c.universe.reify {
       new Field[A] {
         def zero: A = z.splice
         def one: A = o.splice
-        def plus(x: A, y: A): A = ops.plus.splice
-        def times(x: A, y: A): A = ops.times.splice
-        override def minus(x: A, y: A): A = ops.minus.splice
-        def negate(x: A): A = ops.negate.splice
-        def quot(x: A, y: A): A = ops.div.splice
-        def mod(x: A, y: A): A = ops.mod.splice
+        def plus(x: A, y: A): A = ops.plus[A].splice
+        def times(x: A, y: A): A = ops.times[A].splice
+        override def minus(x: A, y: A): A = ops.minus[A].splice
+        def negate(x: A): A = ops.negate[A].splice
+        def quot(x: A, y: A): A = ops.div[A].splice
+        def mod(x: A, y: A): A = ops.mod[A].splice
         def gcd(x: A, y: A): A = euclid(x, y)(ev.splice)
-        def div(x: A, y: A): A = ops.div.splice
+        def div(x: A, y: A): A = ops.div[A].splice
       }
     }
   }
 
   def eqImpl[A: c.WeakTypeTag](c: Context)(): c.Expr[Eq[A]] = {
-    val ops = new AutoOps { val ctx = c }
+    val ops = new ScalaOps { val ctx = c }
     c.universe.reify {
       new Eq[A] {
         def eqv(x: A, y: A): Boolean = ops.equals.splice
@@ -112,7 +112,7 @@ object Auto {
   }
 
   def orderImpl[A: c.WeakTypeTag](c: Context)(): c.Expr[Order[A]] = {
-    val ops = new AutoOps { val ctx = c }
+    val ops = new ScalaOps { val ctx = c }
     c.universe.reify {
       new Order[A] {
         def compare(x: A, y: A): Int = ops.compare.splice
@@ -125,20 +125,20 @@ object Auto {
     def monoid[A](z: A) = macro monoidImpl[A]
 
     def semigroupImpl[A: c.WeakTypeTag](c: Context)(): c.Expr[Semigroup[A]] = {
-      val ops = new AutoOps { val ctx = c }
+      val ops = new ScalaOps { val ctx = c }
       c.universe.reify {
         new Semigroup[A] {
-          def op(x: A, y: A): A = ops.plusplus.splice
+          def op(x: A, y: A): A = ops.plusplus[A].splice
         }
       }
     }
 
     def monoidImpl[A: c.WeakTypeTag](c: Context)(z: c.Expr[A]): c.Expr[Monoid[A]] = {
-      val ops = new AutoOps { val ctx = c }
+      val ops = new ScalaOps { val ctx = c }
       c.universe.reify {
         new Monoid[A] {
           def id: A = z.splice
-          def op(x: A, y: A): A = ops.plusplus.splice
+          def op(x: A, y: A): A = ops.plusplus[A].splice
         }
       }
     }
@@ -156,68 +156,68 @@ object Auto {
     def order[A]() = macro orderImpl[A]
 
     def semiringImpl[A: c.WeakTypeTag](c: Context)(): c.Expr[Semiring[A]] = {
-      val ops = new AutoOps { val ctx = c }
+      val ops = new JavaOps { val ctx = c }
       c.universe.reify {
         new Semiring[A] {
-          def plus(x: A, y: A): A = ops.java.plus.splice
-          def times(x: A, y: A): A = ops.java.times.splice
+          def plus(x: A, y: A): A = ops.plus[A].splice
+          def times(x: A, y: A): A = ops.times[A].splice
         }
       }
     }
 
     def rigImpl[A: c.WeakTypeTag](c: Context)(z: c.Expr[A], o: c.Expr[A]): c.Expr[Rig[A]] = {
-      val ops = new AutoOps { val ctx = c }
+      val ops = new JavaOps { val ctx = c }
       c.universe.reify {
         new Rig[A] {
           def zero: A = z.splice
           def one: A = o.splice
-          def plus(x: A, y: A): A = ops.java.plus.splice
-          def times(x: A, y: A): A = ops.java.times.splice
+          def plus(x: A, y: A): A = ops.plus[A].splice
+          def times(x: A, y: A): A = ops.times[A].splice
         }
       }
     }
 
     def rngImpl[A: c.WeakTypeTag](c: Context)(z: c.Expr[A]): c.Expr[Rng[A]] = {
-      val ops = new AutoOps { val ctx = c }
+      val ops = new JavaOps { val ctx = c }
       c.universe.reify {
         new Rng[A] {
           def zero: A = z.splice
-          def plus(x: A, y: A): A = ops.java.plus.splice
-          def times(x: A, y: A): A = ops.java.times.splice
-          override def minus(x: A, y: A): A = ops.java.minus.splice
-          def negate(x: A): A = ops.java.negate.splice
+          def plus(x: A, y: A): A = ops.plus[A].splice
+          def times(x: A, y: A): A = ops.times[A].splice
+          override def minus(x: A, y: A): A = ops.minus[A].splice
+          def negate(x: A): A = ops.negate[A].splice
         }
       }
     }
 
     def ringImpl[A: c.WeakTypeTag](c: Context)(z: c.Expr[A], o: c.Expr[A]): c.Expr[Ring[A]] = {
-      val ops = new AutoOps { val ctx = c }
+      val ops = new JavaOps { val ctx = c }
       println(ops.hasMethod1[A, A, A]("mod"))
       c.universe.reify {
         new Ring[A] {
           def zero: A = z.splice
           def one: A = o.splice
-          def plus(x: A, y: A): A = ops.java.plus.splice
-          def times(x: A, y: A): A = ops.java.times.splice
-          override def minus(x: A, y: A): A = ops.java.minus.splice
-          def negate(x: A): A = ops.java.negate.splice
+          def plus(x: A, y: A): A = ops.plus[A].splice
+          def times(x: A, y: A): A = ops.times[A].splice
+          override def minus(x: A, y: A): A = ops.minus[A].splice
+          def negate(x: A): A = ops.negate[A].splice
         }
       }
     }
 
     def euclideanRingImpl[A: c.WeakTypeTag](c: Context)
         (z: c.Expr[A], o: c.Expr[A])(ev: c.Expr[Eq[A]]): c.Expr[EuclideanRing[A]] = {
-      val ops = new AutoOps { val ctx = c }
+      val ops = new JavaOps { val ctx = c }
       c.universe.reify {
         new EuclideanRing[A] {
           def zero: A = z.splice
           def one: A = o.splice
-          def plus(x: A, y: A): A = ops.java.plus.splice
-          def times(x: A, y: A): A = ops.java.times.splice
-          override def minus(x: A, y: A): A = ops.java.minus.splice
-          def negate(x: A): A = ops.java.negate.splice
-          def quot(x: A, y: A): A = ops.java.div.splice
-          def mod(x: A, y: A): A = ops.java.mod[A].splice
+          def plus(x: A, y: A): A = ops.plus[A].splice
+          def times(x: A, y: A): A = ops.times[A].splice
+          override def minus(x: A, y: A): A = ops.minus[A].splice
+          def negate(x: A): A = ops.negate[A].splice
+          def quot(x: A, y: A): A = ops.div[A].splice
+          def mod(x: A, y: A): A = ops.mod[A].splice
           def gcd(x: A, y: A): A = euclid(x, y)(ev.splice)
         }
       }
@@ -225,37 +225,37 @@ object Auto {
 
     def fieldImpl[A: c.WeakTypeTag](c: Context)
         (z: c.Expr[A], o: c.Expr[A])(ev: c.Expr[Eq[A]]): c.Expr[Field[A]] = {
-      val ops = new AutoOps { val ctx = c }
+      val ops = new JavaOps { val ctx = c }
       c.universe.reify {
         new Field[A] {
           def zero: A = z.splice
           def one: A = o.splice
-          def plus(x: A, y: A): A = ops.java.plus.splice
-          def times(x: A, y: A): A = ops.java.times.splice
-          override def minus(x: A, y: A): A = ops.java.minus.splice
-          def negate(x: A): A = ops.java.negate.splice
-          def quot(x: A, y: A): A = ops.java.div.splice
-          def mod(x: A, y: A): A = ops.java.mod[A].splice
+          def plus(x: A, y: A): A = ops.plus[A].splice
+          def times(x: A, y: A): A = ops.times[A].splice
+          override def minus(x: A, y: A): A = ops.minus[A].splice
+          def negate(x: A): A = ops.negate[A].splice
+          def quot(x: A, y: A): A = ops.div[A].splice
+          def mod(x: A, y: A): A = ops.mod[A].splice
           def gcd(x: A, y: A): A = euclid(x, y)(ev.splice)
-          def div(x: A, y: A): A = ops.java.div.splice
+          def div(x: A, y: A): A = ops.div[A].splice
         }
       }
     }
 
     def eqImpl[A: c.WeakTypeTag](c: Context)(): c.Expr[Eq[A]] = {
-      val ops = new AutoOps { val ctx = c }
+      val ops = new JavaOps { val ctx = c }
       c.universe.reify {
         new Eq[A] {
-          def eqv(x: A, y: A): Boolean = ops.java.equals.splice
+          def eqv(x: A, y: A): Boolean = ops.equals.splice
         }
       }
     }
 
     def orderImpl[A: c.WeakTypeTag](c: Context)(): c.Expr[Order[A]] = {
-      val ops = new AutoOps { val ctx = c }
+      val ops = new JavaOps { val ctx = c }
       c.universe.reify {
         new Order[A] {
-          def compare(x: A, y: A): Int = ops.java.compare.splice
+          def compare(x: A, y: A): Int = ops.compare.splice
         }
       }
     }
@@ -287,35 +287,6 @@ abstract class AutoOps {
   val ctx: Context
   import ctx.universe._
 
-  def plusplus = binop("$plus$plus")
-  def plus[A] = binop[A]("$plus")
-  def minus[A] = binop[A]("$minus")
-  def times[A] = binop[A]("$times")
-  def negate[A] = unop[A]("unary_$minus")
-  def div[A] = binop[A]("$div")
-  def mod[A] = binop[A]("$percent")
-  def equals = binop[Boolean]("$eq$eq")
-  def compare = binop[Int]("compare")
-
-  object java {
-    def plus[A] = binop[A]("add")
-    def minus[A] = binop[A]("subtract")
-    def times[A] = binop[A]("multiply")
-    def div[A] = binop[A]("divide")
-    def negate[A] = unop[A]("negate")
-    def mod[A: ctx.WeakTypeTag] =
-      if (hasMethod1[A, A, A]("mod")) {
-        binop[A]("mod")
-      } else if (hasMethod1[A, A, A]("remainder")) {
-        binop[A]("remainder")
-      } else {
-        ctx.abort(ctx.enclosingPosition,
-          "Cannot find suitable mod (%) method (checked: mod, remainder).")
-      }
-    def equals = binop[Boolean]("equals")
-    def compare = binop[Int]("compareTo")
-  }
-
   def unop[A](name: String, x: String = "x"): ctx.Expr[A] =
     ctx.Expr[A](Select(Ident(newTermName(x)), newTermName(name)))
 
@@ -342,4 +313,46 @@ abstract class AutoOps {
 
     matches exists { x => x }
   }
+}
+
+abstract class MathOps extends AutoOps {
+  def plus[A: ctx.WeakTypeTag]: ctx.Expr[A]
+  def minus[A: ctx.WeakTypeTag]: ctx.Expr[A]
+  def times[A: ctx.WeakTypeTag]: ctx.Expr[A]
+  def negate[A: ctx.WeakTypeTag]: ctx.Expr[A]
+  def div[A: ctx.WeakTypeTag]: ctx.Expr[A]
+  def mod[A: ctx.WeakTypeTag]: ctx.Expr[A]
+  def equals: ctx.Expr[Boolean]
+  def compare: ctx.Expr[Int]
+}
+
+abstract class ScalaOps extends MathOps {
+  def plusplus[A] = binop[A]("$plus$plus")
+  def plus[A: ctx.WeakTypeTag] = binop[A]("$plus")
+  def minus[A: ctx.WeakTypeTag] = binop[A]("$minus")
+  def times[A: ctx.WeakTypeTag] = binop[A]("$times")
+  def negate[A: ctx.WeakTypeTag] = unop[A]("unary_$minus")
+  def div[A: ctx.WeakTypeTag] = binop[A]("$div")
+  def mod[A: ctx.WeakTypeTag] = binop[A]("$percent")
+  def equals = binop[Boolean]("$eq$eq")
+  def compare = binop[Int]("compare")
+}
+
+abstract class JavaOps extends MathOps {
+  def plus[A: ctx.WeakTypeTag] = binop[A]("add")
+  def minus[A: ctx.WeakTypeTag] = binop[A]("subtract")
+  def times[A: ctx.WeakTypeTag] = binop[A]("multiply")
+  def div[A: ctx.WeakTypeTag] = binop[A]("divide")
+  def negate[A: ctx.WeakTypeTag] = unop[A]("negate")
+  def mod[A: ctx.WeakTypeTag] =
+    if (hasMethod1[A, A, A]("mod")) {
+      binop[A]("mod")
+    } else if (hasMethod1[A, A, A]("remainder")) {
+      binop[A]("remainder")
+    } else {
+      ctx.abort(ctx.enclosingPosition,
+        "Cannot find suitable mod (%) method (checked: mod, remainder).")
+    }
+  def equals = binop[Boolean]("equals")
+  def compare = binop[Int]("compareTo")
 }
