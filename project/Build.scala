169d9f3a224c70cb42a3ea0b32e59b2ad5387895
Erik Osheim <d_m@plastic-idolatry.com>
2013-04-06 18:39:45 -0400

    bump to M4

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ object MyBuild extends Build {
 
   override lazy val settings = super.settings ++ Seq(
     organization := "org.spire-math",
-    version := "0.4.0-M3",
+    version := "0.4.0-M4",
 
     scalaVersion := "2.10.0",
 
