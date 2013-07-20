85222a6d0bc1bfc3e54d16f8b24e97306f1cbc56
Luc J. Bourhis <luc_j_bourhis@mac.com>
2013-07-20 13:53:02 +0200

    Linear algebra package first steps.
    Data structure for matrix storage, matrix blocks and matrix strides.
    Note: no genericity for the type of elements yet.

---
+++ core/src/main/scala/spire/linearAlgebra/Util.scala
@@ -0,0 +1,14 @@
+package spire.linearAlgebra
+
+/**
+ * Constants used throughout this package
+ */
+object Constants {
+
+  /**
+   * Magic number to specify that the end of an index range shall be equal to
+   * the relevant dimenstion (i.e. vector length, number of rows, or columns,
+   * etc)
+   */
+  val End = -111
+}
