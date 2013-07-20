16a8642cd6071e88703329f46764070b8e208036
Luc J. Bourhis <luc_j_bourhis@mac.com>
2013-07-20 13:58:50 +0200

    Safe computation of vector Euclidean norm.

---
+++ core/src/main/scala/spire/linearAlgebra/dense/EuclideanNorm.scala
@@ -0,0 +1,66 @@
+package spire.linearAlgebra.dense
+
+import scala.math.sqrt
+
+/**
+ * Computations of Euclidean norms that safely avoids overflows
+ * and precision loss.
+ *
+ * This is key to the robust computation of Householder reflections,
+ * which are central to many algorithms.
+ *
+ * [1] LAPACK Users' Guide.
+ *     E Anderson, Z Bai, Christian H. Bischof, S Blackford, J Demmel,
+ *     J Dongarra, J Du Croz, A Greenbaum, S Hammarling, A McKenney,
+ *     and D Sorensen.
+ *     Society for Industrial and Applied Mathematics,
+ *     Philadelphia, PA, Third.
+ */
+trait EuclideanNorm {
+
+  /**
+   * Euclidean norm of the vector v
+   *
+   * Reference: subroutine DNRM2 in LAPACK [1]
+   */
+  def euclideanNorm(v: Iterable[Double]): Double = {
+    if(v.hasDefiniteSize) {
+      if(v.size == 0) return 0.0
+      else if(v.size == 1) return v.head.abs
+    }
+    var scale = 0.0
+    var sumSquares = 1.0
+    for(x <- v) {
+      if(x != 0.0) {
+        val xa = x.abs
+        if(scale < xa) {
+          val r = scale/xa
+          sumSquares = 1.0 + sumSquares*r*r
+          scale = xa
+        }
+        else {
+          val r = xa/scale
+          sumSquares += r*r
+        }
+      }
+    }
+    scale*sqrt(sumSquares)
+  }
+
+  /**
+   * Euclidean norm of (x,y)
+   *
+   * Reference: subroutine DLAPY2 in LAPACK [1]
+   */
+  def euclideanNorm2D(x:Double, y:Double) = {
+    val (xa, ya) = (x.abs, y.abs)
+    val (min, max) = if(xa < ya) (xa, ya) else (ya, xa)
+    if(min == 0) {
+      max
+    }
+    else {
+      val r = min/max
+      max*sqrt(1.0 + r*r)
+    }
+  }
+}
---
+++ core/src/test/scala/spire/linearAlgebra/dense/EuclideanNormTest.scala
@@ -0,0 +1,43 @@
+package spire.linearAlgebra.dense.FunSuite
+
+import spire.linearAlgebra.dense._
+
+import org.scalatest.FunSuite
+import scala.math.sqrt
+
+class EuclideanNormTest extends FunSuite with EuclideanNorm {
+
+  val m = Matrix(5,3)( 1, 12,    1,
+                       2,  8,    1e-8,
+                       3, -7,    2,
+                       4,  4,   -1,
+                      -5,  2,    1e-7
+                      )
+
+  test("Trivial cases") {
+    val x1 = new  MatrixStrides(m, first=(4,0), step=1, length=1)
+    expectResult(5.0) { euclideanNorm(x1) }
+    val x2 = new  MatrixStrides(m, first=(0,0), step=1, length=0)
+    expectResult(0.0) { euclideanNorm(x2) }
+  }
+
+  test("Decreasing") {
+    val x1 = m.column(1)
+    expectResult(sqrt(277.0)) { euclideanNorm(x1) }
+  }
+
+  test("Increasing") {
+    val x1 = m.column(0)
+    expectResult(sqrt(55.0)) { euclideanNorm(x1) }
+  }
+
+  test("Badly scaled") {
+    val x1 = m.column(2)
+    expectResult(2.4494897427831801599) { euclideanNorm(x1) }
+  }
+
+  test("Dimension 2") {
+    expectResult(5.0) { euclideanNorm2D(4, -3) }
+    expectResult(4.0000000000000000125) { euclideanNorm2D(4, 1e-8) }
+  }
+}
