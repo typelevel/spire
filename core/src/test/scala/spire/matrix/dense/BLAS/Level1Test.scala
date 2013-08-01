5c8d584cb400abfa48abdac4feeb19825589d7bb
Erik Osheim <d_m@plastic-idolatry.com>
2013-08-01 18:38:18 -0400

    get branch passing tests

--- core/src/test/scala/spire/matrix/dense/BLAS/Level1Test.scala
+++ core/src/test/scala/spire/matrix/dense/BLAS/Level1Test.scala
@@ -5,17 +5,17 @@ import spire.matrix.dense._
 
 import org.scalatest.FunSuite
 
-trait BLASLevel1Test extends FunSuite with BLAS.level1.interface {
+trait BLASLevel1Test extends FunSuite with BLAS.level1.Interface {
 
   test("Vector scaling") {
     val m = Matrix(2,4)(1, 2, 3, 4,
                         5, 6, 7, 8)
-    SCALE(3.0, m.column(0))
-    SCALE(-2.0, m.row(1).block(0,3))
+    scale(3.0, m.column(0))
+    scale(-2.0, m.row(1).block(0,3))
     expectResult(Matrix(2,4)(  3,   2,   3, 4,
                              -30, -12, -14, 8)) { m }
   }
 
 }
 
-class NaiveBLASLevel1Test extends BLASLevel1Test with BLAS.level1.naive
+class NaiveBLASLevel1Test extends BLASLevel1Test with BLAS.level1.Naive
--- core/src/test/scala/spire/matrix/dense/BLAS/Level2.scala
+++ core/src/test/scala/spire/matrix/dense/BLAS/Level2.scala
@@ -6,7 +6,7 @@ import spire.matrix.dense.{Matrix,Vector}
 
 import org.scalatest.FunSuite
 
-trait BLASLevel2Test extends FunSuite with BLAS.level2.interface {
+trait BLASLevel2Test extends FunSuite with BLAS.level2.Interface {
 
   test("General Matrix Vector product (GEMV): no transpose") {
     val a = Matrix(3,5)( 2, -2, 1, -1,  2,
@@ -17,16 +17,16 @@ trait BLASLevel2Test extends FunSuite with BLAS.level2.interface {
     val y1 = y.copyToVector
     val alpha = 2
     val beta = -1
-    GEMV(NoTranspose, alpha, a, x, beta, y1)
+    gemv(NoTranspose, alpha, a, x, beta, y1)
     expectResult(Vector(-26, -10, -17)) { y1 }
     val y2 = y.copyToVector
-    GEMV(NoTranspose, alpha=0, a, x, beta, y2)
+    gemv(NoTranspose, alpha=0, a, x, beta, y2)
     expectResult(Vector(-2, -2, -3)) { y2 }
     val y3 = y.copyToVector
-    GEMV(NoTranspose, alpha, a, x, beta=0, y3)
+    gemv(NoTranspose, alpha, a, x, beta=0, y3)
     expectResult(Vector(-24, -8, -14)) { y3 }
     val y4 = y.copyToVector
-    GEMV(NoTranspose, alpha, a, x, beta=1, y4)
+    gemv(NoTranspose, alpha, a, x, beta=1, y4)
     expectResult(Vector(-22, -6, -11)) { y4 }
   }
 
@@ -39,18 +39,18 @@ trait BLASLevel2Test extends FunSuite with BLAS.level2.interface {
     val y1 = y.copyToVector
     val alpha = 2
     val beta = -1
-    GEMV(Transpose, alpha, a, x, beta, y1)
+    gemv(Transpose, alpha, a, x, beta, y1)
     expectResult(Vector(5, -1, 8, 25, -27)) { y1 }
     val y2 = y.copyToVector
-    GEMV(Transpose, alpha=0, a, x, beta, y2)
+    gemv(Transpose, alpha=0, a, x, beta, y2)
     expectResult(Vector(3, -1, 2, -1, -3)) { y2 }
     val y3 = y.copyToVector
-    GEMV(Transpose, alpha, a, x, beta=0, y3)
+    gemv(Transpose, alpha, a, x, beta=0, y3)
     expectResult(Vector(2, 0, 6, 26, -24)) { y3 }
     val y4 = y.copyToVector
-    GEMV(Transpose, alpha, a, x, beta=1, y4)
+    gemv(Transpose, alpha, a, x, beta=1, y4)
     expectResult(Vector(-1, 1, 4, 27, -21)) { y4 }
   }
 }
 
-class NaiveBLASLevel2Test extends BLASLevel2Test with BLAS.level2.naive
+class NaiveBLASLevel2Test extends BLASLevel2Test with BLAS.level2.Naive
