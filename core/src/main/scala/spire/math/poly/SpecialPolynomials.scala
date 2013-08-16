b2e90d0e0b3604077efd1e3ec8a65475493b8b16
James Thompson <jamesrth@usc.edu>
2013-08-15 20:24:10 -0700

    All special polynomials
    Added laguerre polynomials. Added assertion === tests for special
    polynomials too.

--- core/src/main/scala/spire/math/poly/SpecialPolynomials.scala
+++ core/src/main/scala/spire/math/poly/SpecialPolynomials.scala
@@ -14,7 +14,7 @@ object SpecialPolynomials {
 
   // Horner scheme polynomial generator stream
   def hornerScheme[C: Ring: Signed: ClassTag](zero: Polynomial[C], one: Polynomial[C], 
-                      fn: (Polynomial[C], Polynomial[C], Int) => Polynomial[C]): Stream[Polynomial[C]] = {
+                   fn: (Polynomial[C], Polynomial[C], Int) => Polynomial[C]): Stream[Polynomial[C]] = {
     def loop(pnm1: Polynomial[C], pn: Polynomial[C], n: Int = 1): Stream[Polynomial[C]] = {
       pn #:: loop(pn, fn(pn, pnm1, n), n + 1)
     }
@@ -28,6 +28,11 @@ object SpecialPolynomials {
       val b = Polynomial(Map(1 -> f.fromInt(2 * n + 1)))
       val c = Polynomial(Map(0 -> -f.fromInt(n)))
       a * (b * pn + c * pnm1)
+    }
+
+  private[this] def laguerreFn[C: Signed: ClassTag](implicit f: Field[C]): (Polynomial[C], Polynomial[C], Int) => Polynomial[C] = 
+    (pn: Polynomial[C], pnm1: Polynomial[C], n: Int) => {
+      Polynomial(Map(0 -> f.one / f.fromInt(n + 1))) * (Polynomial(Map(0 -> f.fromInt(2 * n + 1), 1 -> -f.one)) * pn - pnm1 * Polynomial(Map(0 -> f.fromInt(n))))
     }
 
   // Chebyshev recurrence function
@@ -51,6 +56,10 @@ object SpecialPolynomials {
   // Legendre polynomials of the first kind
   def legendres[C: Field: Signed: ClassTag](num: Int): Stream[Polynomial[C]] = 
     hornerScheme(Polynomial.one[C], Polynomial.x[C], legendreFn[C]).take(num)
+
+  // Laguerre polynomials
+  def laguerres[C: Signed: ClassTag](num: Int)(implicit f: Field[C]): Stream[Polynomial[C]] = 
+    hornerScheme(Polynomial.one[C], Polynomial(Map(0 -> f.one, 1 -> -f.one)), laguerreFn[C]).take(num)
 
   // Chebyshev polynomials of the first kind
   def chebyshevsFirstKind[C: Ring: Signed: ClassTag](num: Int): Stream[Polynomial[C]] =
