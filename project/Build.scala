cbd787f83c20f6b9a7970b6a098a00b6f503a915
Erik Osheim <d_m@plastic-idolatry.com>
2013-08-23 14:11:11 -0400

    add thyme dependency

--- project/Build.scala
+++ project/Build.scala
@@ -223,6 +223,9 @@ object MyBuild extends Build {
       "org.apfloat" % "apfloat" % "1.6.3",
       "org.jscience" % "jscience" % "4.3.1",
 
+      // thyme
+      "ichi.bench" % "thyme" % "0.1.0" from "http://plastic-idolatry.com/jars/thyme-0.1.0.jar",
+
       // caliper stuff
       "com.google.guava" % "guava" % "r09",
       "com.google.code.java-allocation-instrumenter" % "java-allocation-instrumenter" % "2.0",
