5980d7386e81863de9fe9cf990c61dd046ec9757
Erik Osheim <d_m@plastic-idolatry.com>
2013-08-25 20:45:34 -0400

    Create optional Trig[Rational] instance.

--- core/src/main/scala/spire/optional.scala
+++ core/src/main/scala/spire/optional.scala
@@ -92,3 +92,28 @@ object totalfloat {
   }
   implicit final val TotalDoubleOrder = new TotalDoubleOrder {}
 }
+
+object rationalTrig {
+  implicit val trigRational = new Trig[Rational] {
+    val r180 = Rational(180)
+    import spire.std.double._
+    def acos(a: Rational): Rational = Rational(spire.math.acos(a.toDouble))
+    def asin(a: Rational): Rational = Rational(spire.math.asin(a.toDouble))
+    def atan(a: Rational): Rational = Rational(spire.math.atan(a.toDouble))
+    def atan2(y: Rational,x: Rational): Rational = Rational(spire.math.atan2(y.toDouble, x.toDouble))
+    def cos(a: Rational): Rational = Rational(spire.math.cos(a.toDouble))
+    def cosh(x: Rational): Rational = Rational(spire.math.cosh(x.toDouble))
+    val e: Rational = Rational(spire.math.e)
+    def exp(a: Rational): Rational = Rational(spire.math.exp(a.toDouble))
+    def expm1(a: Rational): Rational = Rational(spire.math.expm1(a.toDouble))
+    def log(a: Rational): Rational = Rational(spire.math.log(a.toDouble))
+    def log1p(a: Rational): Rational = Rational(spire.math.log1p(a.toDouble))
+    val pi: Rational = Rational(spire.math.pi)
+    def sin(a: Rational): Rational = Rational(spire.math.sin(a.toDouble))
+    def sinh(x: Rational): Rational = Rational(spire.math.sinh(x.toDouble))
+    def tan(a: Rational): Rational = Rational(spire.math.tan(a.toDouble))
+    def tanh(x: Rational): Rational = Rational(spire.math.tanh(x.toDouble))
+    def toDegrees(a: Rational): Rational = (a * r180) / pi
+    def toRadians(a: Rational): Rational = (a / r180) * pi
+  }
+}
