92170dd76a5ccffd84299c2e06747dc51ab7b265
Erik Osheim <d_m@plastic-idolatry.com>
2013-09-02 17:20:55 -0400

    fix badly-merged file

--- macros/src/main/scala/spire/macros/Syntax.scala
+++ macros/src/main/scala/spire/macros/Syntax.scala
@@ -1,168 +1,114 @@
-package spire.macrosk
+package spire.macros
 
 import scala.reflect.macros.Context
 
+case class SyntaxUtil[C <: Context with Singleton](val c: C) {
+  import c.universe._
+  import definitions._
+  import Flag._
+
+  def name(s: String) = newTermName(c.fresh(s + "$"))
+
+  def isClean(es: c.Expr[_]*): Boolean =
+    es.forall {
+      _.tree match {
+        case t @ Ident(_: TermName) if t.symbol.asTerm.isStable => true
+        case Function(_, _) => true
+        case _ => false
+      }
+    }
+}
+
+class InlineUtil[C <: Context with Singleton](val c: C) {
+  import c.universe._
+
+  def inlineAndReset[T](tree: Tree): c.Expr[T] =
+    c.Expr[T](c.resetAllAttrs(inlineApplyRecursive(tree)))
+
+  def inlineApplyRecursive(tree: Tree): Tree = {
+    val ApplyName = newTermName("apply")
+
+    class InlineSymbol(symbol: Symbol, value: Tree) extends Transformer {
+      override def transform(tree: Tree): Tree = tree match {
+        case Ident(_) if tree.symbol == symbol =>
+          value
+        case tt: TypeTree if tt.original != null =>
+          super.transform(TypeTree().setOriginal(transform(tt.original)))
+        case _ =>
+          super.transform(tree)
+      }
+    }
+
+    object InlineApply extends Transformer {
+      def inlineSymbol(symbol: Symbol, body: Tree, arg: Tree): Tree =
+        new InlineSymbol(symbol, arg).transform(body)
+
+      override def transform(tree: Tree): Tree = tree match {
+        case Apply(Select(Function(List(param), body), ApplyName), List(arg)) =>
+          inlineSymbol(param.symbol, body, arg)
+          
+        case Apply(Function(List(param), body), List(arg)) =>
+          inlineSymbol(param.symbol, body, arg)
+          
+        case _ =>
+          super.transform(tree)
+      }
+    }
+
+    InlineApply.transform(tree)
+  }
+}
+
 object Syntax {
 
-  // def rangeMacro(c: Context)(start: c.Expr[Int], limit: c.Expr[Int])(body: c.Expr[Int => Unit]): c.Expr[Unit] = {
-  //   import c.universe._
-  //   import definitions._
-  //   import Flag._
-
-  //   val u = new Util[c.type](c)
-
-  //   val expr = if (u.isClean(body.tree))
-  //     reify {
-  //       {
-  //         var __i = start.splice
-  //         val __limit = limit.splice
-  //         while (__i < __limit) { body.splice.apply(__i); __i += 1 }
-  //       }
-  //     }
-  //   else
-  //     reify {
-  //       {
-  //         var __i = start.splice
-  //         val __limit = limit.splice
-  //         val __f = body.splice
-  //         while (__i < __limit) { __f.apply(__i); __i += 1 }
-  //       }
-  //     }
-
-  //   /**
-  //    * Instead of just returning 'tree', we will go ahead and inline
-  //    * anonymous functions which are immediately applied.
-  //    */
-  //   u.inlineAndReset[Unit](expr.tree)
-  // }
-
-  def cforMacro[A: c.WeakTypeTag](c: Context)(init: c.Expr[A])
+  def cforMacro[A](c: Context)(init: c.Expr[A])
      (test: c.Expr[A => Boolean], next: c.Expr[A => A])
      (body: c.Expr[A => Unit]): c.Expr[Unit] = {
+
     import c.universe._
-    import definitions._
-    import Flag._
+    val util = SyntaxUtil[c.type](c)
+    val index = util.name("index")
 
-    val u = new Util[c.type](c)
-
-    val c.WeakTypeTag(tpe) = implicitly[c.WeakTypeTag[A]]
-
-    def name(s: String) = newTermName(c.fresh(s + "$cfor"))
-
-    val clean = u.isClean(test.tree) && u.isClean(next.tree) && u.isClean(body.tree)
-
-    val tree = if (clean) {
-      /**
-       * If our arguments are all "clean" (anonymous functions or
-       * simple identifiers) then we can go ahead and just inline
-       * them directly into a while loop.
-       */
-      val index = name("index")
-      val whileLoop = name("while")
-      Block(
-        List(
-          ValDef(Modifiers(MUTABLE), index, TypeTree(tpe), init.tree),
-          LabelDef(
-            whileLoop,
-            List(),
-            If(
-              Apply(test.tree, List(Ident(index))),
-              Block(
-                List(
-                  Apply(body.tree, List(Ident(index))),
-                  Assign(Ident(index), Apply(next.tree, List(Ident(index)))),
-                  Apply(Ident(whileLoop), List())),
-                Literal(Constant(()))
-              ),
-              Literal(Constant(()))))),
-        Literal(Constant(())))
+    /**
+     * If our arguments are all "clean" (anonymous functions or simple
+     * identifiers) then we can go ahead and just inline them directly
+     * into a while loop.
+     * 
+     * If one or more of our arguments are "dirty" (something more
+     * complex than an anonymous function or simple identifier) then
+     * we will go ahead and bind each argument to a val just to be
+     * safe.
+     */
+    val tree = if (util.isClean(test, next, body)) {
+      q"""
+      var $index = $init
+      while ($test($index)) {
+        $body($index)
+        $index = $next($index)
+      }
+      """
 
     } else {
+      val testName = util.name("test")
+      val nextName = util.name("next")
+      val bodyName = util.name("body")
 
-      /**
-       * If one or more of our arguments are "dirty" (something more
-       * complex than an anonymous function or simple identifier) then
-       * we will go ahead and bind each argument to a val just to be
-       * safe.
-       */
-      val testName = name("test")
-      val nextName = name("next")
-      val bodyName = name("body")
-
-      val f1 = Select(Select(Ident(nme.ROOTPKG), newTermName("scala")), newTypeName("Function1"))
-      val unit = Select(Select(Ident(nme.ROOTPKG), newTermName("scala")), newTypeName("Unit"))
-      val bool = Select(Select(Ident(nme.ROOTPKG), newTermName("scala")), newTypeName("Boolean"))
-
-      val index = name("index")
-      val whileLoop = name("while")
-      Block(
-        List(
-          ValDef(Modifiers(), testName, AppliedTypeTree(f1, List(TypeTree(tpe), bool)), test.tree),
-          ValDef(Modifiers(), nextName, AppliedTypeTree(f1, List(TypeTree(tpe), TypeTree(tpe))), next.tree),
-          ValDef(Modifiers(), bodyName, AppliedTypeTree(f1, List(TypeTree(tpe), unit)), body.tree),
-          ValDef(Modifiers(MUTABLE), index, TypeTree(tpe), init.tree),
-          LabelDef(
-            whileLoop,
-            List(),
-            If(
-              Apply(Ident(testName), List(Ident(index))),
-              Block(
-                List(
-                  Apply(Ident(bodyName), List(Ident(index))),
-                  Assign(Ident(index), Apply(Ident(nextName), List(Ident(index)))),
-                  Apply(Ident(whileLoop), List())),
-                Literal(Constant(()))
-              ),
-              Literal(Constant(()))))),
-        Literal(Constant(())))
+      q"""
+      val $testName: Int => Boolean = $test
+      val $nextName: Int => Int = $next
+      val $bodyName: Int => Unit = $body
+      var $index: Int = $init
+      while ($testName($index)) {
+        $bodyName($index)
+        $index = $nextName($index)
+      }
+      """
     }
 
     /**
      * Instead of just returning 'tree', we will go ahead and inline
      * anonymous functions which are immediately applied.
      */
-    u.inlineAndReset[Unit](tree)
-  }
-
-  class Util[C <: Context with Singleton](val c:C) {
-    import c.universe._
-
-    def isClean(t: Tree): Boolean = t match {
-      case Ident(_: TermName) if t.symbol.asTerm.isStable => true
-      case Function(_, _) => true
-      case _ => false
-    }
-
-    def inlineAndReset[T](tree: Tree): c.Expr[T] =
-      c.Expr[T](c.resetAllAttrs(inlineApplyRecursive(tree)))
-
-    def inlineApplyRecursive(tree: Tree): Tree = {
-      val ApplyName = newTermName("apply")
-
-      class InlineSymbol(symbol: Symbol, value: Tree) extends Transformer {
-        override def transform(tree: Tree): Tree = tree match {
-          case Ident(_) if tree.symbol == symbol => value
-          case _ => super.transform(tree)
-        }
-      }
-
-      object InlineApply extends Transformer {
-        def inlineSymbol(symbol: Symbol, body: Tree, arg: Tree): Tree =
-          new InlineSymbol(symbol, arg).transform(body)
-
-        override def transform(tree: Tree): Tree = tree match {
-          case Apply(Select(Function(List(param), body), ApplyName), List(arg)) =>
-            inlineSymbol(param.symbol, body, arg)
-  
-          case Apply(Function(List(param), body), List(arg)) =>
-            inlineSymbol(param.symbol, body, arg)
-  
-          case _ =>
-            super.transform(tree)
-        }
-      }
-
-      InlineApply.transform(tree)
-    }
+    new InlineUtil[c.type](c).inlineAndReset[Unit](tree)
   }
 }
