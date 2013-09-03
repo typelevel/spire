6dc6559e7e0814f54745a7427fd5fe216076af10
Erik Osheim <d_m@plastic-idolatry.com>
2013-09-03 00:54:23 -0400

    Added cforRange and cforRange2
    These macros are a bit nicer to use than cfor, although they are also
    less flexible. The basic idea is:
    
        cforRange(0 until 100 by 4) { i =>
          ...
        }
    
    becomes:
    
        var i = 0
        while (i < 100) {
          ...
          i += 4
        }
    
    The following kinds of ranges are supported:
    
        start until limit
        start until limit by step
        start to end
        start to end by step
    
    Non-literal ranges are also supported, but will issue a warning, since
    they can be slower. The warning may need to be removed, we'll see.
    
    The cforRange2 macro is similar:
    
        cforRange2(1 to 5, 2 to 4) { (i, j) =>
          ...
        }
    
    becomes:
    
        cforRange(1 to 5) { i =>
          cforRange(2 to 4) { j =>
            ...
          }
        }
    
    and then:
    
        var i = 1
        while (i <= 5) {
          var j = 2
          while (j <= 4) {
            ...
            j += 1
          }
          i += 1
        }
    
    That's all there is to it.
    
    Conflicts:
    	core/src/main/scala/spire/syntax/Syntax.scala

--- macros/src/main/scala/spire/macros/Syntax.scala
+++ macros/src/main/scala/spire/macros/Syntax.scala
@@ -22,8 +22,10 @@ case class SyntaxUtil[C <: Context with Singleton](val c: C) {
 class InlineUtil[C <: Context with Singleton](val c: C) {
   import c.universe._
 
-  def inlineAndReset[T](tree: Tree): c.Expr[T] =
-    c.Expr[T](c.resetAllAttrs(inlineApplyRecursive(tree)))
+  def inlineAndReset[T](tree: Tree): c.Expr[T] = {
+    val inlined = inlineApplyRecursive(tree)
+    c.Expr[T](c.resetAllAttrs(inlined))
+  }
 
   def inlineApplyRecursive(tree: Tree): Tree = {
     val ApplyName = newTermName("apply")
@@ -44,12 +46,16 @@ class InlineUtil[C <: Context with Singleton](val c: C) {
         new InlineSymbol(symbol, arg).transform(body)
 
       override def transform(tree: Tree): Tree = tree match {
-        case Apply(Select(Function(List(param), body), ApplyName), List(arg)) =>
-          inlineSymbol(param.symbol, body, arg)
+        case Apply(Select(Function(params, body), ApplyName), args) =>
+          params.zip(args).foldLeft(body) { case (b, (param, arg)) =>
+            inlineSymbol(param.symbol, b, arg)
+          }
           
-        case Apply(Function(List(param), body), List(arg)) =>
-          inlineSymbol(param.symbol, body, arg)
-          
+        case Apply(Function(params, body), args) =>
+          params.zip(args).foldLeft(body) { case (b, (param, arg)) =>
+            inlineSymbol(param.symbol, b, arg)
+          }
+
         case _ =>
           super.transform(tree)
       }
@@ -108,7 +114,96 @@ object Syntax {
     /**
      * Instead of just returning 'tree', we will go ahead and inline
      * anonymous functions which are immediately applied.
-     */
+v     */
     new InlineUtil[c.type](c).inlineAndReset[Unit](tree)
+  }
+
+  def cforRangeMacro(c: Context)(r: c.Expr[Range])(body: c.Expr[Int => Unit]): c.Expr[Unit] = {
+
+    import c.universe._
+    val util = SyntaxUtil[c.type](c)
+    val index = util.name("index")
+
+    val IntWrapperName = newTermName("intWrapper")
+    val ToName = newTermName("to")
+    val UntilName = newTermName("until")
+    val ByName = newTermName("by")
+
+    val tree = r.tree match {
+      case Apply(Select(Apply(Select(_, IntWrapperName), List(start)), UntilName), List(limit)) =>
+        val limitName = util.name("limit")
+q"""
+var $index: Int = $start
+val $limitName: Int = $limit
+while ($index < $limitName) {
+  $body($index)
+  $index += 1
+}
+"""
+
+      case Apply(Select(Apply(Select(Apply(Select(_, IntWrapperName), List(start)), UntilName), List(limit)), ByName), List(step)) =>
+        val limitName = util.name("limit")
+        val stepName = util.name("step")
+q"""
+var $index: Int = $start
+val $limitName: Int = $limit
+val $stepName: Int = $step
+while ($index < $limitName) {
+  $body($index)
+  $index += $stepName
+}
+"""
+
+      case Apply(Select(Apply(Select(_, IntWrapperName), List(start)), ToName), List(end)) =>
+        val endName = util.name("end")
+        q"""
+var $index: Int = $start
+val $endName: Int = $end
+while ($index <= $endName) {
+  $body($index)
+  $index += 1
+}
+"""
+
+      case Apply(Select(Apply(Select(Apply(Select(_, IntWrapperName), List(start)), ToName), List(end)), ByName), List(step)) =>
+        val endName = util.name("end")
+        val stepName = util.name("step")
+        q"""
+var $index: Int = $start
+val $endName: Int = $end
+val $stepName: Int = $step
+while ($index <= $endName) {
+  $body($index)
+  $index += $stepName
+}
+"""
+
+      case r =>
+        c.info(c.enclosingPosition, "non-literal range found", true)
+        println(showRaw(r))
+        val range = util.name("range")
+        val limit = util.name("limit")
+        val step = util.name("step")
+
+        q"""
+val $range = $r
+var $index: Int = $range.start
+val $step: Int = $range.step
+val $limit: Int = if ($range.isInclusive) $range.end + 1 else $range.end
+while ($index < $limit) {
+  $body($index)
+  $index += $step
+}
+"""
+    }
+
+    new InlineUtil[c.type](c).inlineAndReset[Unit](tree)
+  }
+
+  def cforRange2Macro(c: Context)(r1: c.Expr[Range], r2: c.Expr[Range])
+    (body: c.Expr[(Int, Int) => Unit]): c.Expr[Unit] = {
+
+    import c.universe._
+    c.Expr[Unit](q"cforRange($r1)(i => cforRange($r2)(j => $body(i, j)))")
   }
 }
