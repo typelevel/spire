115399a06f248cde64d05286ed896003f819310f
James Thompson <jamesrth@usc.edu>
2013-10-08 14:36:26 -0700

    Beginnings of Monomial and Multivariate Polynonials Tests

--- core/src/main/scala/spire/math/poly/MultivariatePolynomial.scala
+++ core/src/main/scala/spire/math/poly/MultivariatePolynomial.scala
@@ -82,7 +82,7 @@ class MultivariatePolynomial[@spec(Double) C] private[spire] (val terms: Array[M
     if(isZero) MultivariatePolynomial.zero[C] else 
       MultivariatePolynomial[C](terms.map(_.:/(headCoefficient)))
 
-  def sum(l: Array[Monomial[C]], r: Array[Monomial[C]])(implicit ring: Semiring[C], eq: Eq[Monomial[C]]) = {
+  private final def sum(l: Array[Monomial[C]], r: Array[Monomial[C]])(implicit ring: Semiring[C], eq: Eq[Monomial[C]]) = {
     val a = ArrayBuilder.make[Monomial[C]]()
     val ar = new Array[Boolean](r.length)
     cfor(0)(_ < ar.length, _ + 1) { x => ar(x) = false }
@@ -117,13 +117,14 @@ class MultivariatePolynomial[@spec(Double) C] private[spire] (val terms: Array[M
 
   // EuclideanRing ops
   def +(rhs: MultivariatePolynomial[C])(implicit r: Semiring[C], eq: Eq[Monomial[C]], eqc: Eq[C]): MultivariatePolynomial[C] =
-    MultivariatePolynomial[C](sum(lhs.terms, rhs.terms))
+    if(rhs == MultivariatePolynomial.zero[C]) lhs else MultivariatePolynomial[C](simplify(sum(lhs.terms, rhs.terms)))
 
   def -(rhs: MultivariatePolynomial[C])(implicit r: Rng[C], eq: Eq[Monomial[C]], eqc: Eq[C]): MultivariatePolynomial[C] =
-    MultivariatePolynomial[C](sum(lhs.terms, (rhs.unary_-).terms))
+    if(lhs == rhs) MultivariatePolynomial.zero[C] else MultivariatePolynomial[C](simplify(sum(lhs.terms, (-rhs).terms)))
 
-  def *(rhs: MultivariatePolynomial[C])(implicit r: Semiring[C], eq: Eq[Monomial[C]], eqc: Eq[C]): MultivariatePolynomial[C] =
-    MultivariatePolynomial[C](simplify(lhs.terms.flatMap(lt => rhs.terms.map(rt => lt * rt))))
+  def *(rhs: MultivariatePolynomial[C])(implicit r: Ring[C], eq: Eq[Monomial[C]], eqc: Eq[C]): MultivariatePolynomial[C] =
+    if(rhs == MultivariatePolynomial.one[C]) lhs else if(rhs == MultivariatePolynomial.zero[C]) MultivariatePolynomial.zero[C]
+      else MultivariatePolynomial[C](simplify(lhs.terms.flatMap(lt => rhs.terms.map(rt => lt * rt))))
 
   def /~(rhs: MultivariatePolynomial[C])(implicit f: Field[C], eq: Eq[C]): MultivariatePolynomial[C] = 
     lhs./%(rhs)._1
@@ -132,21 +133,24 @@ class MultivariatePolynomial[@spec(Double) C] private[spire] (val terms: Array[M
     lhs./%(rhs)._2
 
   def /%(rhs: MultivariatePolynomial[C])(implicit f: Field[C], eq: Eq[C], ct: ClassTag[C]) = {
+  
     @tailrec def quotMod_(quot: MultivariatePolynomial[C],
                           dividend: MultivariatePolynomial[C],
                           divisor: MultivariatePolynomial[C]): (MultivariatePolynomial[C], MultivariatePolynomial[C]) = {
-      if(lhs == rhs) (MultivariatePolynomial.one[C], MultivariatePolynomial.zero[C])
-        else if(divisor.isEmpty || dividend.isEmpty) (quot, dividend) else { // if we can't divide anything in, give it back the quot and dividend
-          if(divisor.head.divides(dividend.head)) {
-            val divTerm = MultivariatePolynomial[C](dividend.head / divisor.head) // the first division
-            val prod = divisor.tail * divTerm // then multiply the rhs.tail by the MVP containing only this product.
-            val quotSum = quot + divTerm // expand the quotient with the divided term
-            val rem = dividend.tail - prod // then subtract from the original dividend tail
-            if(rem.isZero) (quotSum, rem) else quotMod_(quotSum, rem, divisor) // repeat
-          } else quotMod_(quot, dividend, divisor.tail)
+      if(divisor.isEmpty || dividend.isEmpty) (quot, dividend) else { // if we can't divide anything in, give it back the quot and dividend
+        if(divisor.head.divides(dividend.head)) {
+          val divTerm = MultivariatePolynomial[C](dividend.head / divisor.head) // the first division
+          val prod = divisor.tail * divTerm // then multiply the rhs.tail by the MVP containing only this product.
+          val quotSum = quot + divTerm // expand the quotient with the divided term
+          val rem = dividend.tail - prod // then subtract from the original dividend tail
+          if(rem.isZero) (quotSum, rem) else quotMod_(quotSum, rem, divisor) // repeat
+        } else quotMod_(quot, dividend, divisor.tail)
       }
     }
-    quotMod_(MultivariatePolynomial.zero[C], lhs, rhs)
+
+    if(lhs == rhs) (MultivariatePolynomial.one[C], MultivariatePolynomial.zero[C]) else if(rhs == MultivariatePolynomial.one[C]) {
+      (lhs, MultivariatePolynomial.zero[C]) } else if(rhs == MultivariatePolynomial.zero[C]) { (lhs, MultivariatePolynomial.zero[C]) } 
+        else quotMod_(MultivariatePolynomial.zero[C], lhs, rhs)
   }
 
   // VectorSpace ops
