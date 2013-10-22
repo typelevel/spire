3f18f2345a28a4202d3878c35f57f263dd0bd46e
Erik Osheim <d_m@plastic-idolatry.com>
2013-10-22 16:27:11 -0400

    Clean up pull request, and add tests.
    I did a few things here:
    
     * Removed tab characters
     * Improved efficiency a bit with inlined private methods
     * Removed "dangerous" intToByte and longToByte
     * Added ScalaCheck test
     * Fixed a bug that I found
    
    I decided that intToByte and longToByte were "dangerous" since there
    aren't very many "correct" ways of calling them (only 4/8 valid second
    arguments, respectively). Also, by using the new private inlined
    methods I can avoid doing a bunch of arithmetic.
    
    Thanks to flavuit for the pull request and the efficiency improvements!

--- core/src/main/scala/spire/util/Pack.scala
+++ core/src/main/scala/spire/util/Pack.scala
@@ -2,141 +2,139 @@ package spire.util
 
 import java.nio.ByteBuffer
 
+/**
+ * These methods are all big-endian.
+ * 
+ * That is, bytes[0] is the most-significant byte.
+ */
 object Pack {
-	def longToBytes(n: Long): Array[Byte] = {
-		val arr = new Array[Byte](8)
-		arr(0) = longToByte(n, 0)
-		arr(1) = longToByte(n, 1)
-		arr(2) = longToByte(n, 2)
-		arr(3) = longToByte(n, 3)
-		arr(4) = longToByte(n, 4)
-		arr(5) = longToByte(n, 5)
-		arr(6) = longToByte(n, 6)
-		arr(7) = longToByte(n, 7)
-		arr
-	}
 
-	def intToBytes(n: Int): Array[Byte] = {
-		val arr = new Array[Byte](4)
-		arr(0) = intToByte(n, 0)
-		arr(1) = intToByte(n, 1)
-		arr(2) = intToByte(n, 2)
-		arr(3) = intToByte(n, 3)
-		arr
-	}
+  @inline private[this] def ism(n: Int, shift: Int): Byte =
+    ((n >>> shift) & 0xff).toByte
 
-	def longToByte(n: Long, index: Byte): Byte = ((n >>> (64 - ((index + 1) * 8))) & 0xff).toByte
+  def intToBytes(n: Int): Array[Byte] = {
+    val arr = new Array[Byte](4)
+    arr(0) = ism(n, 24)
+    arr(1) = ism(n, 16)
+    arr(2) = ism(n, 8)
+    arr(3) = ism(n, 0)
+    arr
+  }
 
-	def intToByte(n: Int, index: Byte): Byte = ((n >>> (32 - ((index + 1) * 8))) & 0xff).toByte
+  def intsToBytes(ints: Array[Int]): Array[Byte] = {
+    val arr = new Array[Byte](ints.length * 4)
+    var i = 0
+    var j = 0
+    while (i < ints.length) {
+      val n = ints(i)
+      arr(j) = ism(n, 24)
+      arr(j + 1) = ism(n, 16)
+      arr(j + 2) = ism(n, 8)
+      arr(j + 3) = ism(n, 0)
+      i += 1
+      j += 4
+    }
+    arr
+  }
 
-	def intsToBytes(ints: Array[Int]): Array[Byte] = {
-		val len = ints.length
-		val arr = new Array[Byte](len * 4)
-		var i = 0
-		var j = 0
-		while (i < len) {
-			val n = arr(i)
-			arr(j) = intToByte(n, 0)
-			arr(j + 1) = intToByte(n, 1)
-			arr(j + 2) = intToByte(n, 2)
-			arr(j + 3) = intToByte(n, 3)
-			i += 1
-			j += 4
-		}
-		arr
-	}
+  def intFromBytes(bytes: Array[Byte]): Int =
+    intFromByteBuffer(ByteBuffer.wrap(bytes))
 
-	def longsToBytes(ints: Array[Long]): Array[Byte] = {
-		val len = ints.length
-		val arr = new Array[Byte](len * 8)
-		var i = 0
-		var j = 0
-		while (i < len) {
-			val n = arr(i)
-			arr(j) = longToByte(n, 0)
-			arr(j + 1) = longToByte(n, 1)
-			arr(j + 2) = longToByte(n, 2)
-			arr(j + 3) = longToByte(n, 3)
-			arr(j + 4) = longToByte(n, 4)
-			arr(j + 5) = longToByte(n, 5)
-			arr(j + 6) = longToByte(n, 6)
-			arr(j + 7) = longToByte(n, 7)
-			i += 1
-			j += 8
-		}
-		arr
-	}
+  def intFromBytes(b1: Byte, b2: Byte, b3: Byte, b4: Byte) =
+    (b1 & 0xff) << 24 | (b2 & 0xff) << 16 | (b3 & 0xff) << 8 | (b4 & 0xff)
 
-	def intFromBytes(bytes: Array[Byte]): Int =
-		intFromByteBuffer(ByteBuffer.wrap(bytes))
-
-	def intFromBytes(b1: Byte, b2: Byte, b3: Byte, b4: Byte) =
-		b1 << 24 |
-			b2 << 16 |
-			b3 << 8 |
-			b4
-
-	def intFromByteBuffer(bb: ByteBuffer): Int =
-		if (bb.remaining >= 4) {
+  def intFromByteBuffer(bb: ByteBuffer): Int =
+    if (bb.remaining >= 4) {
       bb.getInt()
-		} else {
-			var n = 0
-			while (bb.remaining > 0) n = (n << 8) | bb.get
-			n
-		}
+    } else {
+      var n = 0
+      while (bb.remaining > 0) n = (n << 8) | bb.get
+      n
+    }
 
-	def longFromBytes(bytes: Array[Byte]): Long =
-		longFromByteBuffer(ByteBuffer.wrap(bytes))
+  def intsFromBytes(bytes: Array[Byte], n: Int): Array[Int] =
+    intsFromByteBuffer(ByteBuffer.wrap(bytes), n)
 
-	def longFromBytes(b1: Byte, b2: Byte, b3: Byte, b4: Byte, b5: Byte, b6: Byte, b7: Byte, b8: Byte) =
-		b1 << 56 |
-			b2 << 48 |
-			b3 << 40 |
-			b4 << 32 |
-			b5 << 24 |
-			b6 << 16 |
-			b7 << 8 |
-			b8
-
-	def longFromByteBuffer(bb: ByteBuffer): Long =
-		if (bb.remaining >= 8) {
-      bb.getLong()
-		} else {
-			var n = 0L
-			while (bb.remaining > 0) n = (n << 8) | bb.get
-			n
-		}
-
-	def bytesFromByteBuffer(bb: ByteBuffer, n: Int): Array[Byte] = {
-		val out = new Array[Byte](n)
-		if (bb.remaining >= n) {
-			bb.get(out)
-		} else {
-			var i = 0
-			while (bb.remaining > 0) {out(i) = bb.get; i += 1}
-		}
-		out
-	}
-
-	def intsFromBytes(bytes: Array[Byte], n: Int): Array[Int] =
-		intsFromByteBuffer(ByteBuffer.wrap(bytes), n)
-
-	def intsFromByteBuffer(bb: ByteBuffer, n: Int): Array[Int] = {
-		val out = new Array[Int](n)
-		var i = 0
+  def intsFromByteBuffer(bb: ByteBuffer, n: Int): Array[Int] = {
+    val out = new Array[Int](n)
+    var i = 0
     while (i < n && bb.remaining >= 4) { out(i) = bb.getInt(); i += 1 }
-		if (i < n && bb.remaining > 0) out(i) = intFromByteBuffer(bb)
-		out
-	}
+    if (i < n && bb.remaining > 0) out(i) = intFromByteBuffer(bb)
+    out
+  }
 
-	def longsFromBytes(bytes: Array[Byte], n: Int): Array[Long] =
-		longsFromByteBuffer(ByteBuffer.wrap(bytes), n)
+  @inline private[this] def lsm(n: Long, shift: Int): Byte =
+    ((n >>> shift) & 0xffL).toByte
 
-	def longsFromByteBuffer(bb: ByteBuffer, n: Int): Array[Long] = {
-		val out = new Array[Long](n)
-		var i = 0
+  def longToBytes(n: Long): Array[Byte] = {
+    val arr = new Array[Byte](8)
+    arr(0) = lsm(n, 56)
+    arr(1) = lsm(n, 48)
+    arr(2) = lsm(n, 40)
+    arr(3) = lsm(n, 32)
+    arr(4) = lsm(n, 24)
+    arr(5) = lsm(n, 16)
+    arr(6) = lsm(n, 8)
+    arr(7) = lsm(n, 0)
+    arr
+  }
+
+  def longsToBytes(longs: Array[Long]): Array[Byte] = {
+    val arr = new Array[Byte](longs.length * 8)
+    var i = 0
+    var j = 0
+    while (i < longs.length) {
+      val n = longs(i)
+      arr(j) = lsm(n, 56)
+      arr(j + 1) = lsm(n, 48)
+      arr(j + 2) = lsm(n, 40)
+      arr(j + 3) = lsm(n, 32)
+      arr(j + 4) = lsm(n, 24)
+      arr(j + 5) = lsm(n, 16)
+      arr(j + 6) = lsm(n, 8)
+      arr(j + 7) = lsm(n, 0)
+      i += 1
+      j += 8
+    }
+    arr
+  }
+
+  def longFromBytes(bytes: Array[Byte]): Long =
+    longFromByteBuffer(ByteBuffer.wrap(bytes))
+
+  def longFromBytes(b1: Byte, b2: Byte, b3: Byte, b4: Byte, b5: Byte, b6: Byte, b7: Byte, b8: Byte) =
+    (b1 & 0xffL) << 56 | (b2 & 0xffL) << 48 | (b3 & 0xffL) << 40 |
+      (b4 & 0xffL) << 32 | (b5 & 0xffL) << 24 | (b6 & 0xffL) << 16 |
+      (b7 & 0xffL) << 8 | (b8 & 0xffL)
+
+  def longFromByteBuffer(bb: ByteBuffer): Long =
+    if (bb.remaining >= 8) {
+      bb.getLong()
+    } else {
+      var n = 0L
+      while (bb.remaining > 0) n = (n << 8) | bb.get
+      n
+    }
+
+  def longsFromBytes(bytes: Array[Byte], n: Int): Array[Long] =
+    longsFromByteBuffer(ByteBuffer.wrap(bytes), n)
+
+  def longsFromByteBuffer(bb: ByteBuffer, n: Int): Array[Long] = {
+    val out = new Array[Long](n)
+    var i = 0
     while (i < n && bb.remaining >= 8) { out(i) = bb.getLong(); i += 1 }
-		if (i < n && bb.remaining > 0) out(i) = longFromByteBuffer(bb)
-		out
-	}
+    if (i < n && bb.remaining > 0) out(i) = longFromByteBuffer(bb)
+    out
+  }
+
+  def bytesFromByteBuffer(bb: ByteBuffer, n: Int): Array[Byte] = {
+    val out = new Array[Byte](n)
+    if (bb.remaining >= n) {
+      bb.get(out)
+    } else {
+      var i = 0
+      while (bb.remaining > 0) {out(i) = bb.get; i += 1}
+    }
+    out
+  }
 }
