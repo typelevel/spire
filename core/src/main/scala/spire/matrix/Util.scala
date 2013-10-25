7adacf104420ea6cb53fe4babf354e204ddd2a6e
Luc J. Bourhis <luc_j_bourhis@mac.com>
2013-10-25 16:48:06 +0200

    Better documentation with precise references to DLAMCH in LAPACK

--- core/src/main/scala/spire/matrix/Util.scala
+++ core/src/main/scala/spire/matrix/Util.scala
@@ -15,11 +15,20 @@ object Constants {
 
 /**
  * Typeclass representing the properties of Double
+ *
+ * [1] LAPACK Users' Guide.
+ *     E Anderson, Z Bai, Christian H. Bischof, S Blackford, J Demmel,
+ *     J Dongarra, J Du Croz, A Greenbaum, S Hammarling, A McKenney,
+ *     and D Sorensen.
+ *     Society for Industrial and Applied Mathematics,
+ *     Philadelphia, PA, Third.
  */
 trait NumericPropertiesOfDouble {
     /**
      * Relative epsilon machine a la LAPACK, i.e. 1/2 of the definition
      * used in ISO C/C++. C.f. [1] for a decent introduction.
+     *
+     * DLAMCH('Epsilon') in [1]
      *
      * [1] http://en.wikipedia.org/wiki/Machine_epsilon#Approximation_using_Java
      */
@@ -27,6 +36,8 @@ trait NumericPropertiesOfDouble {
 
     /**
      * Safe minimum s such that 1/s does not overflow
+     *
+     * DLAMCH('Safe Minimum') in [1]
      */
     val safeMinimum = {
       val sfmin = java.lang.Double.MIN_NORMAL
@@ -35,10 +46,18 @@ trait NumericPropertiesOfDouble {
       else sfmin
     }
 
-    /** Base used for the binary representation of mantissa and exponent */
+    /**
+     * Base used for the binary representation of mantissa and exponent
+     *
+     * DLAMCH('Base') in [1]
+     */
     val base = 2
 
-    /** Precision */
+    /**
+     * Precision
+     *
+     * DLAMCH('Precision') in [1]
+     */
     val precision = base*epsilonMachine
 
     /** The next number greater than this is +Infinity */
