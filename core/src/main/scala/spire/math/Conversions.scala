265d3b7fe1b1e3e6630a7890482dc0cccba4a308
Erik Osheim <d_m@plastic-idolatry.com>
2014-01-06 18:05:45 -0500

    wip

--- core/src/main/scala/spire/math/Conversions.scala
+++ core/src/main/scala/spire/math/Conversions.scala
@@ -80,7 +80,7 @@ object Conversion {
         Polynomial(a.terms.map { case Term(c, e) => Term(ev.convert(c), e) })
     }
 
-  // UByte
+  // UByte conversions
   implicit val UByteToUShort = new Conversion[UByte, UShort] { def convert(n: UByte): UShort = UShort(n.toInt) }
   implicit val UByteToUInt = new Conversion[UByte, UInt] { def convert(n: UByte): UInt = UInt(n.toInt) }
   implicit val UByteToULong = new Conversion[UByte, ULong] { def convert(n: UByte): ULong = ULong(n.toInt) }
@@ -97,7 +97,7 @@ object Conversion {
   implicit val UByteToAlgebraic = new Conversion[UByte, Algebraic] { def convert(n: UByte): Algebraic = Algebraic(n.toInt) }
   implicit val UByteToReal = new Conversion[UByte, Real] { def convert(n: UByte): Real = Real(n.toInt) }
 
-  // UShort
+  // UShort conversions
   implicit val UShortToUInt = new Conversion[UShort, UInt] { def convert(n: UShort): UInt = UInt(n.toInt) }
   implicit val UShortToULong = new Conversion[UShort, ULong] { def convert(n: UShort): ULong = ULong(n.toInt) }
   implicit val UShortToNatural = new Conversion[UShort, Natural] { def convert(n: UShort): Natural = Natural(n.toInt) }
@@ -112,7 +112,7 @@ object Conversion {
   implicit val UShortToAlgebraic = new Conversion[UShort, Algebraic] { def convert(n: UShort): Algebraic = Algebraic(n.toInt) }
   implicit val UShortToReal = new Conversion[UShort, Real] { def convert(n: UShort): Real = Real(n.toInt) }
 
-  // UInt
+  // UInt conversions
   implicit val UIntToULong = new Conversion[UInt, ULong] { def convert(n: UInt): ULong = ULong(n.toLong) }
   implicit val UIntToNatural = new Conversion[UInt, Natural] { def convert(n: UInt): Natural = Natural(n.toLong) }
   implicit val UIntToShort = new Conversion[UInt, Short] { def convert(n: UInt): Short = n.toShort }
@@ -125,7 +125,7 @@ object Conversion {
   implicit val UIntToAlgebraic = new Conversion[UInt, Algebraic] { def convert(n: UInt): Algebraic = Algebraic(n.toLong) }
   implicit val UIntToReal = new Conversion[UInt, Real] { def convert(n: UInt): Real = Real(n.toLong) }
 
-  // ULong
+  // ULong conversions
   implicit val ULongToNatural = new Conversion[ULong, Natural] { def convert(n: ULong): Natural = n.toNatural }
   implicit val ULongToBigInt = new Conversion[ULong, BigInt] { def convert(n: ULong): BigInt = n.toBigInt }
   implicit val ULongToSafeLong = new Conversion[ULong, SafeLong] { def convert(n: ULong): SafeLong = n.toSafeLong }
@@ -134,7 +134,7 @@ object Conversion {
   implicit val ULongToAlgebraic = new Conversion[ULong, Algebraic] { def convert(n: ULong): Algebraic = Algebraic(n.toSafeLong) }
   implicit val ULongToReal = new Conversion[ULong, Real] { def convert(n: ULong): Real = Real(n.toSafeLong) }
 
-  // Natural
+  // Natural conversions
   implicit val NaturalToBigInt = new Conversion[Natural, BigInt] { def convert(n: Natural): BigInt = n.toBigInt }
   implicit val NaturalToSafeLong = new Conversion[Natural, SafeLong] { def convert(n: Natural): SafeLong = SafeLong(n.toBigInt) }
   implicit val NaturalToBigDecimal = new Conversion[Natural, BigDecimal] { def convert(n: Natural): BigDecimal = BigDecimal(n.toBigInt) }
@@ -142,7 +142,7 @@ object Conversion {
   implicit val NaturalToAlgebraic = new Conversion[Natural, Algebraic] { def convert(n: Natural): Algebraic = Algebraic(n.toBigInt) }
   implicit val NaturalToReal = new Conversion[Natural, Real] { def convert(n: Natural): Real = Real(n.toBigInt) }
 
-  // Byte
+  // Byte conversions
   implicit val ByteToShort = new Conversion[Byte, Short] { def convert(n: Byte): Short = n.toShort }
   implicit val ByteToInt = new Conversion[Byte, Int] { def convert(n: Byte): Int = n.toInt }
   implicit val ByteToLong = new Conversion[Byte, Long] { def convert(n: Byte): Long = n.toLong }
@@ -155,7 +155,7 @@ object Conversion {
   implicit val ByteToAlgebraic = new Conversion[Byte, Algebraic] { def convert(n: Byte): Algebraic = Algebraic(n) }
   implicit val ByteToReal = new Conversion[Byte, Real] { def convert(n: Byte): Real = Real(n) }
 
-  // Short
+  // Short conversions
   implicit val ShortToInt = new Conversion[Short, Int] { def convert(n: Short): Int = n.toInt }
   implicit val ShortToLong = new Conversion[Short, Long] { def convert(n: Short): Long = n.toLong }
   implicit val ShortToBigInt = new Conversion[Short, BigInt] { def convert(n: Short): BigInt = BigInt(n) }
@@ -167,7 +167,7 @@ object Conversion {
   implicit val ShortToAlgebraic = new Conversion[Short, Algebraic] { def convert(n: Short): Algebraic = Algebraic(n) }
   implicit val ShortToReal = new Conversion[Short, Real] { def convert(n: Short): Real = Real(n) }
 
-  // Int
+  // Int conversions
   implicit val IntToLong = new Conversion[Int, Long] { def convert(n: Int): Long = n.toLong }
   implicit val IntToBigInt = new Conversion[Int, BigInt] { def convert(n: Int): BigInt = BigInt(n) }
   implicit val IntToSafeLong = new Conversion[Int, SafeLong] { def convert(n: Int): SafeLong = SafeLong(n) }
@@ -177,7 +177,7 @@ object Conversion {
   implicit val IntToAlgebraic = new Conversion[Int, Algebraic] { def convert(n: Int): Algebraic = Algebraic(n) }
   implicit val IntToReal = new Conversion[Int, Real] { def convert(n: Int): Real = Real(n) }
 
-  // Long
+  // Long conversions
   implicit val LongToBigInt = new Conversion[Long, BigInt] { def convert(n: Long): BigInt = BigInt(n) }
   implicit val LongToSafeLong = new Conversion[Long, SafeLong] { def convert(n: Long): SafeLong = SafeLong(n) }
   implicit val LongToBigDecimal = new Conversion[Long, BigDecimal] { def convert(n: Long): BigDecimal = BigDecimal(n) }
@@ -185,39 +185,39 @@ object Conversion {
   implicit val LongToAlgebraic = new Conversion[Long, Algebraic] { def convert(n: Long): Algebraic = Algebraic(n) }
   implicit val LongToReal = new Conversion[Long, Real] { def convert(n: Long): Real = Real(n) }
 
-  // BigInt
+  // BigInt conversions
   implicit val BigIntToSafeLong = new Conversion[BigInt, SafeLong] { def convert(n: BigInt): SafeLong = SafeLong(n) }
   implicit val BigIntToBigDecimal = new Conversion[BigInt, BigDecimal] { def convert(n: BigInt): BigDecimal = BigDecimal(n) }
   implicit val BigIntToRational = new Conversion[BigInt, Rational] { def convert(n: BigInt): Rational = Rational(n) }
   implicit val BigIntToAlgebraic = new Conversion[BigInt, Algebraic] { def convert(n: BigInt): Algebraic = Algebraic(n) }
   implicit val BigIntToReal = new Conversion[BigInt, Real] { def convert(n: BigInt): Real = Real(n) }
 
-  // SafeLong
+  // SafeLong conversions
   implicit val SafeLongToBigInt = new Conversion[SafeLong, BigInt] { def convert(n: SafeLong): BigInt = n.toBigInt }
   implicit val SafeLongToBigDecimal = new Conversion[SafeLong, BigDecimal] { def convert(n: SafeLong): BigDecimal = BigDecimal(n.toBigInt) }
   implicit val SafeLongToRational = new Conversion[SafeLong, Rational] { def convert(n: SafeLong): Rational = Rational(n) }
   implicit val SafeLongToAlgebraic = new Conversion[SafeLong, Algebraic] { def convert(n: SafeLong): Algebraic = Algebraic(n) }
   implicit val SafeLongToReal = new Conversion[SafeLong, Real] { def convert(n: SafeLong): Real = Real(n) }
 
-  // Float
+  // Float conversions
   implicit val FloatToDouble = new Conversion[Float, Double] { def convert(n: Float): Double = n.toDouble }
   implicit val FloatToBigDecimal = new Conversion[Float, BigDecimal] { def convert(n: Float): BigDecimal = BigDecimal(n) }
   implicit val FloatToRational = new Conversion[Float, Rational] { def convert(n: Float): Rational = Rational(n) }
   implicit val FloatToAlgebraic = new Conversion[Float, Algebraic] { def convert(n: Float): Algebraic = Algebraic(n) }
   implicit val FloatToReal = new Conversion[Float, Real] { def convert(n: Float): Real = Real(n) }
 
-  // Double
+  // Double conversions
   implicit val DoubleToBigDecimal = new Conversion[Double, BigDecimal] { def convert(n: Double): BigDecimal = BigDecimal(n) }
   implicit val DoubleToRational = new Conversion[Double, Rational] { def convert(n: Double): Rational = Rational(n) }
   implicit val DoubleToAlgebraic = new Conversion[Double, Algebraic] { def convert(n: Double): Algebraic = Algebraic(n) }
   implicit val DoubleToReal = new Conversion[Double, Real] { def convert(n: Double): Real = Real(n) }
 
-  // BigDecimal
+  // BigDecimal conversions
   implicit val BigDecimalToRational = new Conversion[BigDecimal, Rational] { def convert(n: BigDecimal): Rational = Rational(n) }
   implicit val BigDecimalToAlgebraic = new Conversion[BigDecimal, Algebraic] { def convert(n: BigDecimal): Algebraic = Algebraic(n) }
   implicit val BigDecimalToReal = new Conversion[BigDecimal, Real] { def convert(n: BigDecimal): Real = Real(n) }
 
-  // Rational
+  // Rational conversions
   implicit val RationalToAlgebraic = new Conversion[Rational, Algebraic] { def convert(n: Rational): Algebraic = Algebraic(n) }
   implicit val RationalToReal = new Conversion[Rational, Real] { def convert(n: Rational): Real = Real(n) }
 }
@@ -226,6 +226,7 @@ object UnsafeConversion {
   def apply[@sp(Byte, Short, Int, Long, Float, Double) A, @sp(Byte, Short, Int, Long, Float, Double) B](f: A => B): UnsafeConversion[A, B] =
     new UnsafeConversion[A, B] { def convert(a: A): B = f(a) }
 
+  // unsafe Int conversions
   implicit val IntToUByte = new UnsafeConversion[Int, UByte] { def convert(n: Int): UByte = UByte(n) }
   implicit val IntToUShort = new UnsafeConversion[Int, UShort] { def convert(n: Int): UShort = UShort(n) } 
   implicit val IntToUInt = new UnsafeConversion[Int, UInt] { def convert(n: Int): UInt = UInt(n) } 
@@ -235,6 +236,7 @@ object UnsafeConversion {
   implicit val IntToShort = new UnsafeConversion[Int, Short] { def convert(n: Int): Short = n.toShort }
   implicit val IntToFloat = new UnsafeConversion[Int, Float] { def convert(n: Int): Float = n.toFloat }
 
+  // unsafe Long conversions
   implicit val LongToUByte = new UnsafeConversion[Long, UByte] { def convert(n: Long): UByte = UByte(n.toInt) }
   implicit val LongToUShort = new UnsafeConversion[Long, UShort] { def convert(n: Long): UShort = UShort(n.toInt) } 
   implicit val LongToUInt = new UnsafeConversion[Long, UInt] { def convert(n: Long): UInt = UInt(n.toInt) }
