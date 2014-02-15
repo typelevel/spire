660dc75c0612d2fab5133d427926fb4427bddeb1
Erik Osheim <d_m@plastic-idolatry.com>
2014-02-15 14:43:01 -0500

    Add basic sketch of a random monad.
    Dist[A] is already a monad but requires a generator to be threaded
    through it to produce values. The new Random[A, G] monad is also
    parameterized on a generator, wraps a Dist[A], and has methods to
    automatically thread the generator context through.
    
    This hopefully frees us from the immutable Generator types.

---
+++ core/src/main/scala/spire/random/mutable/Serial.scala
@@ -0,0 +1,23 @@
+package spire.random
+package mutable
+
+import spire.util.Pack
+import spire.math._
+import java.nio.ByteBuffer
+
+final class Serial(seed0: Long) extends LongBasedGenerator {
+  private[this] var seed: Long = seed0
+  def copyInit: Serial = new Serial(seed)
+  def getSeed: Long = seed
+  def setSeed(n: Long): Unit = seed = n
+  def getSeedBytes: Array[Byte] = Pack.longToBytes(seed)
+  def setSeedBytes(bytes: Array[Byte]): Unit = seed = Pack.longFromBytes(bytes)
+  def nextLong(): Long = { seed += 1; seed }
+}
+
+object Serial extends GeneratorCompanion[Serial, Long] {
+  def randomSeed(): Long = System.nanoTime()
+  def fromBytes(bytes: Array[Byte]): Serial = new Serial(Pack.longFromBytes(bytes))
+  def fromSeed(seed: Long): Serial = new Serial(seed)
+  def fromTime(time: Long = System.nanoTime()): Serial = new Serial(time)
+}
