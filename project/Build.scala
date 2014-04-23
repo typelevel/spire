c4d85ba62070df81f01e051ce772c7e1d4a1837e
Erik Osheim <d_m@plastic-idolatry.com>
2014-04-23 10:46:53 -0400

    upgrade macro paradise to 2.0.0

--- project/Build.scala
+++ project/Build.scala
@@ -73,16 +73,18 @@ object MyBuild extends Build {
 
     resolvers += Resolver.sonatypeRepo("snapshots"),
     resolvers += Resolver.sonatypeRepo("releases"),
+
     libraryDependencies := {
       CrossVersion.partialVersion(scalaVersion.value) match {
         // if scala 2.11+ is used, quasiquotes are merged into scala-reflect
         case Some((2, scalaMajor)) if scalaMajor >= 11 =>
           libraryDependencies.value
+
         // in Scala 2.10, quasiquotes are provided by macro-paradise
         case Some((2, 10)) =>
           libraryDependencies.value ++ Seq(
-            compilerPlugin("org.scalamacros" % "paradise" % "2.0.0-M6" cross CrossVersion.full),
-            "org.scalamacros" %% "quasiquotes" % "2.0.0-M6" cross CrossVersion.full)
+            compilerPlugin("org.scalamacros" % "paradise" % "2.0.0" cross CrossVersion.full),
+            "org.scalamacros" %% "quasiquotes" % "2.0.0")
       }
     },
 
