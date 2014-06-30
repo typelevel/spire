51504d3be9c4ecad3a3cc95c5f87aff143dcd23b
Denis Rosset <denis.rosset@unige.ch>
2014-06-30 23:13:50 +0200

    Interval partial order.

--- core/src/main/scala/spire/math/Interval.scala
+++ core/src/main/scala/spire/math/Interval.scala
@@ -941,11 +941,49 @@ object Interval {
       throw new NumberFormatException("For input string: " + s)
   }
 
-  implicit def eq[A: Eq]: Eq[Interval[A]] =
-    new Eq[Interval[A]] {
-      def eqv(x: Interval[A], y: Interval[A]): Boolean =
+  /** Interval partial order defined as follows:
+    *
+    * Involving empty intervals:
+    * 
+    * - if I and J are empty, then I === J.
+    * - if I (resp. J) is empty and J (resp. I) is non-empty,
+    *   the ordering is undefined (preserving antisymmetry).
+    * 
+    * For non-empty intervals:
+    * 
+    * - I === J is standard Eq semantics (I, J are intersubstituable)
+    * - I < J if all x \in I, y \in J have x < y
+    * - I > J if all x \in I, y \in J have x > y
+    */
+  implicit def partialOrder[A: Order]: PartialOrder[Interval[A]] =
+    new PartialOrder[Interval[A]] {
+      override def eqv(x: Interval[A], y: Interval[A]): Boolean =
         (x.lowerPair === y.lowerPair) && (x.upperPair === y.upperPair)
+
+      def partialCompare(i: Interval[A], j: Interval[A]): Double = {
+        import Double.NaN
+        if (eqv(i, j)) return 0
+        if (i.isEmpty || j.isEmpty)
+          return NaN
+
+        // test if i < j
+        (i.upperBound, j.lowerBound) match {
+          case (Open(x), Open(y)) if x <= y => return -1
+          case (Open(x), Closed(y)) if x <= y => return -1
+          case (Closed(x), Open(y)) if x <= y => return -1
+          case (Closed(x), Closed(y)) if x < y => return -1
+        }
+        // test if i > j
+          (i.lowerBound, j.upperBound) match {
+          case (Open(x), Open(y)) if x >= y => return 1
+          case (Open(x), Closed(y)) if x >= y => return 1
+          case (Closed(x), Open(y)) if x >= y => return 1
+          case (Closed(x), Closed(y)) if x > y => return 1
+        }
+        return NaN
+      }
     }
+
 
   implicit def semiring[A](implicit ev: Ring[A], o: Order[A]): Semiring[Interval[A]] =
     new Semiring[Interval[A]] {
