551461c5100a6f40057d62202206388587d51e3f
Tom Switzer <thomas.switzer@gmail.com>
2014-07-11 17:14:57 -0400

    Uncomment tests - oops.

--- tests/src/test/scala/spire/laws/LawTests.scala
+++ tests/src/test/scala/spire/laws/LawTests.scala
@@ -27,51 +27,51 @@ class LawTests extends FunSuite with Discipline {
     }
   }
 
-  // // Float and Double fail these tests
-  // checkAll("Int",      RingLaws[Int].euclideanRing)
-  // checkAll("Long",     RingLaws[Long].euclideanRing)
-  // checkAll("BigInt",   RingLaws[BigInt].euclideanRing)
-  // checkAll("Rational", RingLaws[Rational].field)
-  // checkAll("Real",     RingLaws[Real].field)
+  // Float and Double fail these tests
+  checkAll("Int",      RingLaws[Int].euclideanRing)
+  checkAll("Long",     RingLaws[Long].euclideanRing)
+  checkAll("BigInt",   RingLaws[BigInt].euclideanRing)
+  checkAll("Rational", RingLaws[Rational].field)
+  checkAll("Real",     RingLaws[Real].field)
 
-  // checkAll("Levenshtein distance", BaseLaws[String].metricSpace)
-  // checkAll("BigInt",               BaseLaws[BigInt].metricSpace)
+  checkAll("Levenshtein distance", BaseLaws[String].metricSpace)
+  checkAll("BigInt",               BaseLaws[BigInt].metricSpace)
 
-  // // We skip checking all tuple types, as they are all generated from the same
-  // // template.
-  // checkAll("(Int,Int)",           RingLaws[(Int, Int)].ring)
-  // checkAll("(Rational,Rational)", RingLaws[(Rational, Rational)].ring)
+  // We skip checking all tuple types, as they are all generated from the same
+  // template.
+  checkAll("(Int,Int)",           RingLaws[(Int, Int)].ring)
+  checkAll("(Rational,Rational)", RingLaws[(Rational, Rational)].ring)
 
-  // import spire.optional.vectorOrder._
+  import spire.optional.vectorOrder._
 
-  // // Testing all A <: Seq is redundant, as we treat them uniformly via.
-  // // iterators and CanBuildFroms. So, presuming the Scala std lib is tested,
-  // // testing just List and Vector should suffice for us.
+  // Testing all A <: Seq is redundant, as we treat them uniformly via.
+  // iterators and CanBuildFroms. So, presuming the Scala std lib is tested,
+  // testing just List and Vector should suffice for us.
 
-  // checkAll("List[Int]",        VectorSpaceLaws[List[Int], Int].module)
-  // checkAll("Vector[Int]",      VectorSpaceLaws[Vector[Int], Int].module)
-  // checkAll("List[Rational]",   VectorSpaceLaws[List[Rational], Rational].vectorSpace)
-  // checkAll("Vector[Rational]", VectorSpaceLaws[Vector[Rational], Rational].vectorSpace)
-  // 
-  // checkAll("Array[Int]",         VectorSpaceLaws[Array[Int], Int].module)
-  // checkAll("Array[VectorSpace]", VectorSpaceLaws[Array[Rational], Rational].vectorSpace)
+  checkAll("List[Int]",        VectorSpaceLaws[List[Int], Int].module)
+  checkAll("Vector[Int]",      VectorSpaceLaws[Vector[Int], Int].module)
+  checkAll("List[Rational]",   VectorSpaceLaws[List[Rational], Rational].vectorSpace)
+  checkAll("Vector[Rational]", VectorSpaceLaws[Vector[Rational], Rational].vectorSpace)
+  
+  checkAll("Array[Int]",         VectorSpaceLaws[Array[Int], Int].module)
+  checkAll("Array[VectorSpace]", VectorSpaceLaws[Array[Rational], Rational].vectorSpace)
 
-  // checkAll("Map[String,Int]",      VectorSpaceLaws[Map[String,Int], Int].module)
-  // checkAll("Map[String,Rational]", VectorSpaceLaws[Map[String,Rational], Rational].vectorSpace)
+  checkAll("Map[String,Int]",      VectorSpaceLaws[Map[String,Int], Int].module)
+  checkAll("Map[String,Rational]", VectorSpaceLaws[Map[String,Rational], Rational].vectorSpace)
 
-  // val max = NormedVectorSpace.max[Rational, List]
-  // checkAll("List[Rational]",
-  //   VectorSpaceLaws[List[Rational], Rational].normedVectorSpace(max, implicitly, implicitly))
+  val max = NormedVectorSpace.max[Rational, List]
+  checkAll("List[Rational]",
+    VectorSpaceLaws[List[Rational], Rational].normedVectorSpace(max, implicitly, implicitly))
 
-  // checkAll("List[Int]",   GroupLaws[List[Int]].monoid)
-  // checkAll("Vector[Int]", GroupLaws[Vector[Int]].monoid)
-  // checkAll("Set[Int]",    GroupLaws[Set[Int]](spire.optional.genericEq.generic, implicitly).monoid)
-  // checkAll("String[Int]", GroupLaws[String].monoid)
-  // checkAll("Array[Int]",  GroupLaws[Array[Int]].monoid)
+  checkAll("List[Int]",   GroupLaws[List[Int]].monoid)
+  checkAll("Vector[Int]", GroupLaws[Vector[Int]].monoid)
+  checkAll("Set[Int]",    GroupLaws[Set[Int]](spire.optional.genericEq.generic, implicitly).monoid)
+  checkAll("String[Int]", GroupLaws[String].monoid)
+  checkAll("Array[Int]",  GroupLaws[Array[Int]].monoid)
 
-  // checkAll("String", VectorSpaceLaws[String, Int].metricSpace)
+  checkAll("String", VectorSpaceLaws[String, Int].metricSpace)
 
-  // checkAll("Sign", GroupActionLaws[Sign, Int].multiplicativeGroupAction)
+  checkAll("Sign", GroupActionLaws[Sign, Int].multiplicativeGroupAction)
 
   implicit def eqFreeMonoid[A: Monoid: Eq]: Eq[FreeMonoid[A]] = new Eq[FreeMonoid[A]] {
     def eqv(x: FreeMonoid[A], y: FreeMonoid[A]): Boolean =
