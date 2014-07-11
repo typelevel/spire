04b1c12df447146510aa14f4495e23a0b4a7452e
Tom Switzer <thomas.switzer@gmail.com>
2014-07-11 15:01:35 -0400

    Refactor D3 test group a bit.

--- tests/src/test/scala/spire/laws/D3.scala
+++ tests/src/test/scala/spire/laws/D3.scala
@@ -7,30 +7,32 @@ import org.scalacheck.{Arbitrary, Gen}
 /**
  * The smallest non-abelian group, for testing non-abelian groups.
  */
-final case class D3(n: Int, m: Boolean)
+final class D3 private (val n: Int) extends AnyVal
 
 object D3 {
+  final def apply(n: Int): D3 = new D3(n % 6)
+
   implicit object D3Algebra extends Group[D3] with Eq[D3] {
-    def id: D3 = D3(0, false)
+    def id: D3 = new D3(0)
 
     def op(x: D3, y: D3): D3 = {
-      val D3(n1, m) = y
-      x match {
-        case D3(n0, false) => D3((n0 + n1) % 3, m)
-        case D3(n0, true) => D3((n0 + 3 - n1) % 3, !m)
+      val n0 = x.n / 2
+      val n1 = y.n / 2
+      val m = y.n % 2
+
+      if (x.n % 2 == 0) {
+        new D3(((n0 + n1) % 3) * 2 + m)
+      } else {
+        new D3(((n0 + 3 - n1) % 3) * 2 + (m ^ 1))
       }
     }
 
-    def inverse(x: D3): D3 = x match {
-      case D3(_, true) => x
-      case D3(n, false) => D3((3 - n) % 3, false)
-    }
+    def inverse(x: D3): D3 =
+      if (x.n % 2 == 1) x else new D3(((3 - x.n / 2) % 3) * 2)
 
-    def eqv(x: D3, y: D3): Boolean = x == y
+    def eqv(x: D3, y: D3): Boolean = x.n == y.n
   }
 
-  implicit def D3Aribtrary: Arbitrary[D3] = 
-    Arbitrary(Gen.oneOf(0, 1, 2, 3, 4, 5).map { x =>
-      D3(x / 2, x % 2 == 0)
-    })
+  implicit def D3Aribtrary: Arbitrary[D3] =
+    Arbitrary(Gen.oneOf(0, 1, 2, 3, 4, 5).map(D3(_)))
 }
