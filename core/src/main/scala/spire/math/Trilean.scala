5d7960a694346f9d60f7d19c2137e8103778acfb
Erik Osheim <d_m@plastic-idolatry.com>
2014-07-26 18:47:14 -0400

    Respond to @tixxit's review comments.

--- core/src/main/scala/spire/math/Trilean.scala
+++ core/src/main/scala/spire/math/Trilean.scala
@@ -2,52 +2,93 @@ package spire.math
 
 import spire.algebra.Order
 
-class Trilean(val int: Int) extends AnyVal { lhs =>
-  def isTrue: Boolean = int == -1
-  def isFalse: Boolean = int == 0
-  def isUnknown: Boolean = int == 1
+/**
+ * Implementation of three-valued logic.
+ * 
+ * This type resembles Boolean, but has three values instead of two:
+ * 
+ *  - Trilean.True (equivalent to true)
+ *  - Trilean.False (equivalent to false)
+ *  - Trilean.Unknown
+ * 
+ * Trilean supports the same operations that Boolean does, and as long
+ * as all values are True or False, the results will be the
+ * same. However, the truth tables have to be extended to work with
+ * unknown:
+ * 
+ * not:
+ * -+-
+ * T|F
+ * U|U
+ * F|T
+ * 
+ * and:
+ *  |T U F
+ * -+-----
+ * T|T U F
+ * U|U U F
+ * F|F F F
+ * 
+ * or:
+ *  |T U F
+ * -+-----
+ * T|T T T
+ * U|T U U
+ * F|T U F
+ * 
+ * Trilean is implemented as a value type, so in most cases it will
+ * only have the overhead of a single Int. However, in some situations
+ * it will be boxed.
+ */
+class Trilean (val value: Int) extends AnyVal { lhs =>
+  def isTrue: Boolean = value == -1
+  def isFalse: Boolean = value == 0
+  def isUnknown: Boolean = value == 1
 
-  def isKnown: Boolean = int != 1
-  def isNotTrue: Boolean = int != -1
-  def isNotFalse: Boolean = int != 0
+  def isKnown: Boolean = value != 1
+  def isNotTrue: Boolean = value != -1
+  def isNotFalse: Boolean = value != 0
 
-  def fold[A](f: Boolean => A)(g: => A): A =
-    if (int == 1) g else f(int == -1)
+  def fold[A](f: Boolean => A)(unknown: => A): A =
+    if (value == 1) unknown else f(value == -1)
 
   def assumeTrue: Boolean =
-    int != 0
+    value != 0
 
   def assumeFalse: Boolean =
-    int == -1
+    value == -1
+
+  def assume(b: Boolean): Boolean =
+    if (value == 1) b else value == -1
 
   def toBoolean(b: => Boolean): Boolean =
-    if (int == 1) b else int == -1
+    if (value == 1) b else value == -1
 
   def toOption: Option[Boolean] =
-    if (int == 1) None else Some(int == -1)
+    if (value == 1) None else Some(value == -1)
 
   override def toString: String =
-    if (int == -1) "T" else if (int == 0) "F" else "U"
+    if (value == -1) "T" else if (value == 0) "F" else "U"
 
   def &&(rhs: => Trilean): Trilean =
-    if (lhs.int == 0) lhs else lhs & rhs
+    if (lhs.value == 0) lhs else lhs & rhs
 
   def ||(rhs: => Trilean): Trilean =
-    if (lhs.int == -1) lhs else lhs | rhs
+    if (lhs.value == -1) lhs else lhs | rhs
 
   def unary_! : Trilean =
-    if (int == 1) lhs else new Trilean(~lhs.int)
+    if (value == 1) lhs else new Trilean(~lhs.value)
 
   def &(rhs: Trilean): Trilean =
-    new Trilean(lhs.int & rhs.int)
+    new Trilean(lhs.value & rhs.value)
 
   def |(rhs: Trilean): Trilean =
-    new Trilean(lhs.int | rhs.int)
+    new Trilean(lhs.value | rhs.value)
 
   def ^(rhs: Trilean): Trilean =
-    if (lhs.int == 1) lhs
-    else if (rhs.int == 1) rhs
-    else new Trilean(lhs.int ^ rhs.int)
+    if (lhs.value == 1) lhs
+    else if (rhs.value == 1) rhs
+    else new Trilean(lhs.value ^ rhs.value)
 
   def imp(rhs: Trilean): Trilean =
     (!lhs) | rhs
@@ -59,15 +100,15 @@ class Trilean(val int: Int) extends AnyVal { lhs =>
     !(lhs | rhs)
 
   def nxor(rhs: Trilean): Trilean =
-    if (lhs.int == 1) lhs
-    else if (rhs.int == 1) rhs
-    else new Trilean(~(lhs.int ^ rhs.int))
+    if (lhs.value == 1) lhs
+    else if (rhs.value == 1) rhs
+    else new Trilean(~(lhs.value ^ rhs.value))
 }
 
 object Trilean {
-  val True: Trilean = new Trilean(-1)
-  val False: Trilean = new Trilean(0)
-  val Unknown: Trilean = new Trilean(1)
+  final val True: Trilean = new Trilean(-1)
+  final val False: Trilean = new Trilean(0)
+  final val Unknown: Trilean = new Trilean(1)
 
-  def apply(b: Boolean): Trilean = if (b) True else False
+  final def apply(b: Boolean): Trilean = if (b) True else False
 }
