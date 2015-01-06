7b5d2ad567547aec762e2faa64da8d7ac9f1577d
Tom Switzer <thomas.switzer@gmail.com>
2015-01-06 17:24:53 -0500

    Ensure bounds calculations use checked arithmetic.

--- core/src/main/scala/spire/math/Algebraic.scala
+++ core/src/main/scala/spire/math/Algebraic.scala
@@ -1134,7 +1134,7 @@ object Algebraic extends AlgebraicInstances {
       }
     }
 
-    def apply(expr: Algebraic.Expr): Bound = {
+    def apply(expr: Algebraic.Expr): Bound = checked {
       // Unfortunately, we must call degreeBound early, to avoid many redundant
       // traversals of the Expr tree. Getting this out of the way early on
       // means that we will traverse the tree once and populate the degreeBound
