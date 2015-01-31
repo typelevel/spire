a70cbff042b7f12d669345e54ff4c65f2ede8bc2
Tom Switzer <thomas.switzer@gmail.com>
2015-01-31 12:12:40 -0500

    Add docs for NumberTag.

--- core/src/main/scala/spire/math/NumberTag.scala
+++ core/src/main/scala/spire/math/NumberTag.scala
@@ -88,27 +88,103 @@ object NumberTag {
   }
 }
 
+/**
+ * A `NumberTag` provides information about important implementations details
+ * of numbers. For instance, it includes information about whether we can
+ * expect arithmetic to overflow or produce invalid values, the bounds of the
+ * number if they exist, whether it is an approximate or exact number type,
+ * etc.
+ */
 trait NumberTag[A] {
+
+  /**
+   * Returns the resolution of this number.
+   */
   def resolution: NumberTag.Resolution
 
+  /**
+   * Returns the smallest finite value that `A` can represent, if one exists.
+   * For instance, the smallest finite value representable by `Double` is
+   * `-1.7976931348623157E308`. On the other hand, `BigInt` has no smallest
+   * value.
+   */
   def hasMinValue: Option[A]
+
+  /**
+   * If `A` has a value that represents the real value 0, then it is returned
+   * here. Otherwise `None` is returned.
+   */
   def hasZero: Option[A]
+
+  /**
+   * Returns the largest finite value that `A` can represent, if one exists.
+   * For instance, the largest finite value representable by `Double` is
+   * `1.7976931348623157E308`. On the other hand, `BigInt` has no largest
+   * value.
+   */
   def hasMaxValue: Option[A]
 
+  /**
+   * If `A` has values that represent an undefined or invalid value, then a
+   * repsentitive value may be used here. Otherwise this returned `None` to
+   * indicate that all values in `A` are valid numbers in the extended real
+   * number line.
+   */
   def hasNaN: Option[A]
+
+  /**
+   * If `A` has a value that represents a positive infinity, then it is
+   * returned here, otherwise a value of `None` indicates that positive
+   * infinity cannot be represented in `A`.
+   */
   def hasPositiveInfinity: Option[A]
+
+  /**
+   * If `A` has a value that represents a negative infinity, then it is
+   * returned here, otherwise a value of `None` indicates that negative
+   * infinity cannot be represented in `A`.
+   */
   def hasNegativeInfinity: Option[A]
 
+  /**
+   * Returns true if this value can overflow as a result of arithmetic
+   * operations. Types that overflow include `Int` and `Long`.
+   */
   def overflows: Boolean
+
+  /**
+   * Returns true if `A` can represent both positive and negative values.
+   */
   def isSigned: Boolean
 
+  /**
+   * Returns true if all values representable by `A` are finite and live on the
+   * real number line.
+   */
   def finite: Boolean =
     hasMinValue.isDefined && hasMaxValue.isDefined
 
+  /**
+   * Returns true if this type can represent arbitrarily large or small values.
+   */
   def infinite: Boolean =
     hasMinValue.isEmpty || hasMaxValue.isEmpty
 
+  /**
+   * Returns `true` if `a` is an infinite value (either positive or negative)
+   * and false otherwise.
+   */
   def isInfinite(a: A): Boolean
+
+  /**
+   * Returns `true` if `a` is an invalid number. Note that positive and
+   * negative infinities are valid numbers.
+   */
   def isNaN(a: A): Boolean
+
+  /**
+   * Returns `true` if `a` represents a finite value (neither infinite nor
+   * invalid).
+   */
   def isFinite(a: A): Boolean = !(isInfinite(a) || isNaN(a))
 }
