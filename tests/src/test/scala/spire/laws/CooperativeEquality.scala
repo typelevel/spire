1df13fa17d40ebc2336ed2c3e1e5877649567eba
Kevin Meredith <Kevin.m.meredith@gmail.com>
2015-04-19 23:08:14 -0400

    Added comparative equals method call.

--- tests/src/test/scala/spire/laws/CooperativeEquality.scala
+++ tests/src/test/scala/spire/laws/CooperativeEquality.scala
@@ -1,28 +1,36 @@
 package spire.laws
 
-import org.scalacheck.Properties
+import org.scalacheck.{Properties, Arbitrary, Gen}
 import org.scalacheck.Prop.forAll
 import org.scalacheck.Prop._
-import org.scalacheck.Gen
-import org.scalacheck.Arbitrary
 import org.scalacheck.Arbitrary._
 
 import spire.math.Algebraic
 
+/** 
+ * Cooperative Equality = equals and hashCode agree
+ */
 object CooperativeEquality extends Properties("Algebraic") {
 
-    val genAlgebraicInt: Gen[Algebraic] = for {
-    	x <- arbitrary[Int]
-  	} yield Algebraic(x)
+	def equalz[A](x: A, y: A) = x == y && x.hashCode == y.hashCode
 
+	val genAlgebraicInt: Gen[Algebraic] = arbitrary[Int].map(Algebraic(_))
 
   	implicit val arbitraryAlgebraicInt: Arbitrary[Algebraic] = Arbitrary(genAlgebraicInt)
 
 	property("commutative +") = forAll { (a: Algebraic, b: Algebraic) =>
-    	(a+b) == (b+a)
+    	equalz(a+b,b+a) 
   	}
 
 	property("commutative *") = forAll { (a: Algebraic, b: Algebraic) =>
-    	(a*b) == (b*a)
+    	equalz(a*b,b*a) 
+  	}
+
+    val genAlgebraicSameInt: Gen[Algebraic] = Gen.choose(Int.MinValue, Int.MaxValue).map(Algebraic(_))
+
+    implicit val arbitraryAlgebraicSameInt: Arbitrary[Algebraic] = Arbitrary(genAlgebraicSameInt)
+
+	property("tranisitive ==") = forAll { (a: Algebraic, b: Algebraic, c: Algebraic) =>
+      if(a == b && b == c) (a == c) else true
   	}  	
 }
