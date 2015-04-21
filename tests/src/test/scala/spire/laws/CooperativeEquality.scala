5a31026c449f0564c5b5310a313e1228f0ed5e9e
Kevin Meredith <Kevin.m.meredith@gmail.com>
2015-04-20 22:11:46 -0400

    Added Gen argument to forAll.

--- tests/src/test/scala/spire/laws/CooperativeEquality.scala
+++ tests/src/test/scala/spire/laws/CooperativeEquality.scala
@@ -1,6 +1,6 @@
 package spire.laws
 
-import org.scalacheck.{Properties, Arbitrary, Gen}
+import org.scalacheck.{Prop, Properties, Arbitrary, Gen}
 import org.scalacheck.Prop.forAll
 import org.scalacheck.Prop._
 import org.scalacheck.Arbitrary._
@@ -12,25 +12,21 @@ import spire.math.Algebraic
  */
 object CooperativeEquality extends Properties("Algebraic") {
 
-	def equalz[A](x: A, y: A) = x == y && x.hashCode == y.hashCode
+	def coopEquals[A](x: A, y: A) = x == y && x.hashCode == y.hashCode
 
 	val genAlgebraicInt: Gen[Algebraic] = arbitrary[Int].map(Algebraic(_))
 
-  	implicit val arbitraryAlgebraicInt: Arbitrary[Algebraic] = Arbitrary(genAlgebraicInt)
+	property("commutative +") = forAll(genAlgebraicInt, genAlgebraicInt) { (a: Algebraic, b: Algebraic) =>
+  	coopEquals(a+b,b+a) 
+	}
 
-	property("commutative +") = forAll { (a: Algebraic, b: Algebraic) =>
-    	equalz(a+b,b+a) 
-  	}
+	property("commutative *") = forAll(genAlgebraicInt, genAlgebraicInt) { (a: Algebraic, b: Algebraic) =>
+  	coopEquals(a*b,b*a) 
+	}
 
-	property("commutative *") = forAll { (a: Algebraic, b: Algebraic) =>
-    	equalz(a*b,b*a) 
-  	}
+  //TODO: improve since only a handful of the 100 runs could 'truly' evaluate to true
+	property("tranisitive ==") = forAll(genAlgebraicInt, genAlgebraicInt, genAlgebraicInt) { (a: Algebraic, b: Algebraic, c: Algebraic) =>
+    if( coopEquals(a, b) && coopEquals(b, c)) coopEquals(a, c) else true
+	}
 
-    val genAlgebraicSameInt: Gen[Algebraic] = Gen.choose(Int.MinValue, Int.MaxValue).map(Algebraic(_))
-
-    implicit val arbitraryAlgebraicSameInt: Arbitrary[Algebraic] = Arbitrary(genAlgebraicSameInt)
-
-	property("tranisitive ==") = forAll { (a: Algebraic, b: Algebraic, c: Algebraic) =>
-      if(a == b && b == c) (a == c) else true
-  	}  	
 }
