d94115a1591a05cb876b19ec12607d0aa713c254
Kevin Meredith <Kevin.m.meredith@gmail.com>
2015-04-30 14:35:19 -0400

    Added PositiveInt macro.

---
+++ macros/src/main/scala/spire/macros/PositiveInt.scala
@@ -0,0 +1,27 @@
+package spire.macros
+
+import scala.language.experimental.macros
+
+import scala.reflect.macros.blackbox.Context
+
+case class PositiveInt(value: Int) {
+  require(value > 0)
+}
+
+object PositiveInt {
+  implicit def wrapConstantInt(n: Int): PositiveInt = macro verifyPositiveInt
+
+  def verifyPositiveInt(c: Context)(n: c.Expr[Int]): c.Expr[PositiveInt] = {
+    import c.universe._
+
+    val tree = n.tree match {
+      case Literal(Constant(x: Int)) if x > 0 =>
+        q"_root_.spire.math.PositiveInt($n)"
+      case Literal(Constant(x: Int)) =>
+        c.abort(c.enclosingPosition, s"$x <= 0")
+      case _ => 
+        q"_root_.spire.math.PositiveInt($n)"
+    }
+    c.Expr(tree)
+  }
+}
--- tests/src/test/scala/spire/math/JetTest.scala
+++ tests/src/test/scala/spire/math/JetTest.scala
@@ -3,11 +3,14 @@ import org.scalatest._
 import scala.{specialized => sp}
 import spire.algebra._
 import spire.implicits._
+import spire.macros.PositiveInt
 
 class JetTest extends FunSuite with Matchers {
+ 
+  implicit def intToPositiveInt(x: Int): PositiveInt = PositiveInt(x)
 
   // Default test with 3-dimensional Jet's
-  implicit val dim = JetDim(3)
+  implicit val dim: JetDim = JetDim(3)
   val maxError = 1.0e-12
 
   test("JetDim") {
