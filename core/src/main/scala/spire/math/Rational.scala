0511bcbbd6a829d61bbd7dce58650d6b270576d7
Kevin Meredith <Kevin.m.meredith@gmail.com>
2015-05-04 12:01:14 -0400

    LongRational#===.

--- core/src/main/scala/spire/math/Rational.scala
+++ core/src/main/scala/spire/math/Rational.scala
@@ -709,9 +709,12 @@ private[math] object LongRationals extends Rationals[Long] {
     }
 
     override def equals(that: Any): Boolean = that match {
-      case that: LongRational => this.n == that.n && this.d == that.d
+      case that: LongRational => ===(that)
       case _ => super.equals(that)
     }
+
+    def ===(that: LongRational): Boolean =
+      this.n == that.n && this.d == that.d
 
     override def longValue: Long =
       if(d == 1L) n
