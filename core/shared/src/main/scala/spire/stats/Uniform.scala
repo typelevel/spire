8b1b277621163fd5dc3a5343bf31acf067cc15d9
André L. F. Pinto <andreluizferreirapinto@gmail.com>
2015-06-20 21:34:43 -0300

    Create type classes for uniform distribution

--- core/shared/src/main/scala/spire/stats/Uniform.scala
+++ core/shared/src/main/scala/spire/stats/Uniform.scala
@@ -11,9 +11,33 @@ trait UniformDistribution[A] extends ContinuousDistribution[A] {
 }
 
 object Uniform {
-  def Standard[A: Field: IsReal: TypeTag](): UniformDistribution[A] = new StandardUniformDistributionImpl[A]()
+  def Standard[A: Field: IsReal: TypeTag: StandardBuilder](): UniformDistribution[A] = implicitly[StandardBuilder[A]].apply()
 
-  def apply[A: Field: IsReal: TypeTag](min: A, max: A): UniformDistribution[A] = new UniformDistributionImpl[A](min, max)
+  def apply[A: Field: IsReal: TypeTag: Builder](min: A, max: A): UniformDistribution[A] = implicitly[Builder[A]].apply(min, max)
+
+  abstract class StandardBuilder[A: Field: IsReal: TypeTag] {
+    def apply(): UniformDistribution[A]
+  }
+
+  abstract class Builder[A: Field: IsReal: TypeTag] {
+    def apply(min: A, max: A): UniformDistribution[A]
+  }
+
+  object StandardBuilder {
+    import spire.std.double._
+
+    implicit val doubleBuilder = new StandardBuilder[Double] {
+      def apply() = new StandardUniformDistributionImpl()
+    }
+  }
+
+  object Builder {
+    import spire.std.double._
+
+    implicit val doubleBuilder = new Builder[Double] {
+      def apply(min: Double, max: Double) = new UniformDistributionImpl(min, max)
+    }
+  }
 
   class StandardUniformDistributionImpl[A: Field: IsReal: TypeTag] extends UniformDistribution[A] {
     import spire.syntax.isReal._
@@ -36,7 +60,7 @@ object Uniform {
     }
   }
 
-  class UniformDistributionImpl[A: Field: IsReal: TypeTag](val min: A, val max: A) extends UniformDistribution[A] {
+  class UniformDistributionImpl[A: Field: IsReal: TypeTag: StandardBuilder](val min: A, val max: A) extends UniformDistribution[A] {
     import spire.syntax.field._
 
     val family = new LocationScaleFamily[A] {
