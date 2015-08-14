a9da5e27b2d5bd169fdb975a797421db541796d7
Ruediger Klaehn <rklaehn@gmail.com>
2015-08-14 11:06:52 +0200

    Improved benchmarks for Rational and SafeLong

--- benchmark-jmh/src/main/scala/spire/benchmark/jmh/SafeLongBenchmarks.scala
+++ benchmark-jmh/src/main/scala/spire/benchmark/jmh/SafeLongBenchmarks.scala
@@ -5,6 +5,34 @@ import java.util.concurrent.TimeUnit
 import org.openjdk.jmh.annotations._
 import org.openjdk.jmh.infra.Blackhole
 import spire.math.SafeLong
+import SafeLongUtil._
+
+object SafeLongUtil {
+  private def isBig(x: SafeLong) = x.getClass.getSimpleName.endsWith("BigInteger")
+
+  def classify(a: SafeLong): String =
+    if (isBig(a)) "b" else "l"
+
+  def classify(a: SafeLong, op_a: SafeLong): String =
+    classify(a) + "_" + classify(op_a)
+
+  def classify(a: SafeLong, b: SafeLong, a_op_b: SafeLong): String =
+    classify(a) + "_" + classify(b) + "_" + classify(a_op_b)
+
+  def check(cases: Map[String, (SafeLong, SafeLong)]): Unit = {
+    for ((kind, (a, b)) ← cases) {
+      val c = classify(a, b)
+      require(kind.startsWith(c), s"Unexpected class $c for case $kind")
+    }
+  }
+
+  def check(cases: Map[String, (SafeLong, SafeLong)], op: (SafeLong, SafeLong) ⇒ SafeLong): Unit = {
+    for ((kind, (a, b)) ← cases) {
+      val c = classify(a, b, op(a, b))
+      require(kind.startsWith(c), s"Unexpected class $c for case $kind")
+    }
+  }
+}
 
 @BenchmarkMode(Array(Mode.AverageTime))
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
@@ -12,16 +40,17 @@ import spire.math.SafeLong
 class SafeLongMultiplyBenchmark {
 
   val pairs: Map[String, (SafeLong, SafeLong)] = Map(
-    "lll" → ((SafeLong.two, SafeLong.two)),
-    "llb" → ((SafeLong.two, SafeLong.safe64 - 1)),
-    "lbl" → ((SafeLong.minusOne, SafeLong.safe64)),
-    "lbb" → ((SafeLong.two, SafeLong.safe64)),
-    "bll" → ((SafeLong.safe64, SafeLong.minusOne)),
-    "blb" → ((SafeLong.safe64, SafeLong.one)),
-    "bbb" → ((SafeLong.safe64, SafeLong.safe64))
+    "l_l_l" → ((SafeLong.two, SafeLong.two)),
+    "l_l_b" → ((SafeLong.two, SafeLong.safe64 - 1)),
+    "l_b_l" → ((SafeLong.minusOne, SafeLong.safe64)),
+    "l_b_b" → ((SafeLong.two, SafeLong.safe64)),
+    "b_l_l" → ((SafeLong.safe64, SafeLong.minusOne)),
+    "b_l_b" → ((SafeLong.safe64, SafeLong.two)),
+    "b_b_b" → ((SafeLong.safe64, SafeLong.safe64))
   )
+  check(pairs, _ * _)
 
-  @Param(Array("lll", "llb", "lbl", "lbb", "bll", "blb", "bbb"))
+  @Param(Array("l_l_l", "l_l_b", "l_b_l", "l_b_b", "b_l_l", "b_l_b", "b_b_b"))
   var kind: String = ""
 
   var a: SafeLong = 0L
@@ -50,23 +79,22 @@ class SafeLongMultiplyBenchmark {
 class SafeLongAddSubtractBenchmark {
 
   val pairs: Map[String, (SafeLong, SafeLong)] = Map(
-    "lll" → ((SafeLong.one, SafeLong.one)),
-    "llb" → ((SafeLong.one, SafeLong.safe64 - 1)),
-    "lbl" → ((SafeLong.minusOne, SafeLong.safe64)),
-    "lbb" → ((SafeLong.one, SafeLong.safe64)),
-    "bll" → ((SafeLong.safe64, SafeLong.minusOne)),
-    "blb" → ((SafeLong.safe64, SafeLong.one)),
-    "bbl" → ((SafeLong.safe64, -SafeLong.safe64)),
-    "bbb" → ((SafeLong.safe64, SafeLong.safe64))
+    "l_l_l" → ((SafeLong.one, SafeLong.one)),
+    "l_l_b" → ((SafeLong.one, SafeLong.safe64 - 1)),
+    "l_b_l" → ((SafeLong.minusOne, SafeLong.safe64)),
+    "l_b_b" → ((SafeLong.one, SafeLong.safe64)),
+    "b_l_l" → ((SafeLong.safe64, SafeLong.minusOne)),
+    "b_l_b" → ((SafeLong.safe64, SafeLong.one)),
+    "b_b_l" → ((SafeLong.safe64, -SafeLong.safe64 - 1)),
+    "b_b_b" → ((SafeLong.safe64, SafeLong.safe64))
   )
+  check(pairs, _ + _)
+  check(pairs, _ - -_)
 
-  @Param(Array("lll", "llb", "lbl", "lbb", "bll", "blb", "bbl", "bbb"))
+  @Param(Array("l_l_l", "l_l_b", "l_b_l", "l_b_b", "b_l_l", "b_l_b", "b_b_l", "b_b_b"))
   var kind: String = ""
-
   var a: SafeLong = 0L
-
   var b: SafeLong = 0L
-
   var c: SafeLong = 0L
 
   @Setup
@@ -86,5 +114,36 @@ class SafeLongAddSubtractBenchmark {
 
   def subtract(x: Blackhole): Unit = {
     x.consume(a - c)
+  }
+}
+
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Thread)
+class SafeLongCompareBenchmark {
+
+  val pairs: Map[String, (SafeLong, SafeLong)] = Map(
+    "l_l" → ((SafeLong.one, SafeLong.one + 1)),
+    "b_b" → ((SafeLong.safe64, SafeLong.safe64 + 1))
+  )
+  check(pairs)
+
+  @Param(Array("l_l", "b_b"))
+  var kind: String = ""
+  var a: SafeLong = 0L
+  var b: SafeLong = 0L
+
+  @Setup
+  def setup(): Unit = {
+
+
+    val (a0, b0) = pairs(kind)
+    a = a0
+    b = b0
+  }
+
+  @Benchmark
+  def compare(x: Blackhole): Unit = {
+    x.consume(a compare b)
   }
 }
