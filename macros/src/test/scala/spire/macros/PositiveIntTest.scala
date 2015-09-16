92e37076662b8887d27bf64e1a078bf28a1a1d08
kevinmeredith <kevin.m.meredith@gmail.com>
2015-09-15 21:40:38 -0400

    Add property-based tests.

--- macros/src/test/scala/spire/macros/PositiveIntTest.scala
+++ macros/src/test/scala/spire/macros/PositiveIntTest.scala
@@ -4,6 +4,10 @@ import org.scalatest.Matchers
 
 import org.scalatest._
 
+import org.scalacheck.{Prop, Gen}
+import Gen.{posNum, negNum}
+import org.scalacheck.Prop.forAll
+import org.scalacheck.Prop._
 
 /**
  * Verifies that this macro will only compile successfully for `Constant` AST
@@ -11,18 +15,39 @@ import org.scalatest._
  */
 class PositiveIntTest extends FunSuite with Matchers {
 
-  test("Constructing a new instance with a negative value using the case class directly") {
-    intercept[IllegalArgumentException] {
-      PositiveInt(-10)
+  val positiveInteger: Gen[Int] = posNum[Int]
+  val negativeInteger: Gen[Int] = negNum[Int]
+
+  private def throwsUponCall[A, E](f: Function0[A]): Boolean = {
+    try {
+      f.apply
+      false
+    }
+    catch {
+      case _: E => true
+      case _    => false
     }
   }
 
-  test("Constructing a new instance with a negative value using the companion object's method ") {
-      """ import spire.macros.PositiveInt; PositiveInt.build(-555) """.shouldNot(compile)
-  }
+  val shallThrowWithNegNumInCaseClassApply: Prop =
+    forAll(negativeInteger) { i: Int => throwsUponCall( () =>  PositiveInt(i) ) }
 
-  test("Constructing a new instance with a positive value using the companion object's method ") {
-    """ import spire.macros.PositiveInt; PositiveInt.build(42) """.should(compile)
+  val shallNotCompileWithNegConstNumInBuildMethod: Prop =
+    forAll(negativeInteger) {
+      i: Int => s"import spire.macros.PositiveInt; PositiveInt.build(${i}})".shouldNot(compile)
+    }
+
+  val shallReturnPositiveIntForPositiveIntConst: Prop =
+    forAll(positiveInteger) {
+      i: Int => PositiveInt.build(i) === PositiveInt(i)
+    }
+
+  shallThrowWithNegNumInCaseClassApply.check
+  shallNotCompileWithNegConstNumInBuildMethod.check
+  shallReturnPositiveIntForPositiveIntConst.check
+
+  test("Constructing a `PositiveInt` via its case class with a (0 :Int) should throw") {
+    assert( throwsUponCall( () => PositiveInt(0) ) )
   }
 
 }
