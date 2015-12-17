ad486a2849d800f52c88ec6bfc32dfc5506ea475
Denis Rosset <denis.rosset@unige.ch>
2015-12-17 16:56:46 +0100

    Interval: added warning w.r.t. robust arithmetic

--- core/shared/src/main/scala/spire/math/Interval.scala
+++ core/shared/src/main/scala/spire/math/Interval.scala
@@ -45,6 +45,11 @@ import java.lang.Double.isNaN
  * These situations will result in loss of precision (in the form of
  * wider intervals). The result is not wrong per se, but less
  * acccurate than it could be.
+ * 
+ * These intervals should not be used with floating point bounds,
+ * as proper rounding is not implemented. Generally, the JVM is
+ * not an easy platform to perform robust arithmetic, as the
+ * IEEE 754 rounding modes cannot be set.
  */
 sealed abstract class Interval[A](implicit order: Order[A]) { lhs =>
 
