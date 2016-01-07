a0e84c6275efd6abc6daa6a5a8c7f148311f043b
Andre Silva <andre.beat@gmail.com>
2016-01-07 00:55:53 +0000

    implement XorShift128Plus PRNG

---
+++ core/shared/src/main/scala/spire/random/rng/XorShift128Plus.scala
@@ -0,0 +1,43 @@
+package spire
+package random
+package rng
+
+import spire.util.Pack
+
+class XorShift128Plus(private var s0: Long, private var s1: Long) extends LongBasedGenerator {
+  def copyInit: XorShift128Plus = new XorShift128Plus(s0, s1)
+
+  def getSeedBytes: Array[Byte] = Pack.longsToBytes(Array(s0, s1))
+
+  def setSeedBytes(bytes: Array[Byte]): Unit = {
+    val seed = Pack.longsFromBytes(bytes, 2)
+    s0 = seed(0)
+    s1 = seed(1)
+  }
+
+  def nextLong(): Long = {
+    var x = s0
+    val y = s1
+    s0 = y
+    x ^= x << 23
+    s1 = x ^ y ^ (x >>> 17) ^ (y >>> 26)
+    s1 + y
+  }
+}
+
+object XorShift128Plus extends GeneratorCompanion[XorShift128Plus, (Long, Long)] {
+  def randomSeed(): (Long, Long) = (System.nanoTime(), System.nanoTime())
+
+  def fromSeed(seed: (Long, Long)): XorShift128Plus = {
+    val (s0, s1) = seed
+    assert(s0 != 0 || s1 != 0)
+    new XorShift128Plus(s0, s1)
+  }
+
+  def fromBytes(bytes: Array[Byte]): XorShift128Plus = {
+    val seed = Pack.longsFromBytes(bytes, 2)
+    fromSeed((seed(0), seed(1)))
+  }
+
+  def fromTime(time: Long = System.nanoTime()): XorShift128Plus = fromSeed((time, time))
+}
