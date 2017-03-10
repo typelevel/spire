8ae7ff09cc68557926dda45636a7e2c26f25e52c
Denis Rosset <physics@denisrosset.com>
2017-03-10 21:24:08 +0800

    Removed leftover unused imports.

--- core/shared/src/main/scala/spire/algebra/DivisionRing.scala
+++ core/shared/src/main/scala/spire/algebra/DivisionRing.scala
@@ -1,9 +1,6 @@
 package spire
 package algebra
 
-import java.lang.Double.{ isInfinite, isNaN, doubleToLongBits }
-import java.lang.Long.{ numberOfTrailingZeros }
-
 trait DivisionRing[@sp(Byte, Short, Int, Long, Float, Double) A] extends Any with Ring[A] with MultiplicativeGroup[A] { self =>
 
   def fromDouble(a: Double): A = Ring.defaultFromDouble[A](a)(self, self)
