235f4c3ec17d4901d6e1aa85bc76d66a8e4de7a9
Stephen Lazaro <stephen@earnest.com>
2017-11-27 21:37:32 -0800

    Differentiate Left and Right Modules

--- core/shared/src/main/scala/spire/algebra/Module.scala
+++ core/shared/src/main/scala/spire/algebra/Module.scala
@@ -1,16 +1,51 @@
 package spire
 package algebra
 
+/**
+ * Left modules only allow action on the left.
+ */
+trait LeftModule[@sp(Int,Long,Float,Double) R, V] extends Any with AdditiveAbGroup[V] {
+  implicit def scalar: Rng[R]
+
+  def timesl(r: R, v: V): V
+}
+object LeftModule {
+  @inline final def apply[@sp(Int,Long,Float,Double) R, V](implicit V: LeftModule[V, R]): LeftModule[V, R] = V
+
+  implicit def IdentityLeftModule[@sp(Int,Long,Float,Double) V](implicit ring: Ring[V]): IdentityLeftModule[V] = {
+    new IdentityLeftModule[V] {
+      val scalar = ring
+    }
+  }
+}
+
+/**
+ * Right modules only allow action on the right.
+ */
+trait RightModule[V, @sp(Int,Long,Float,Double) R] extends Any with AdditiveAbGroup[V] {
+  implicit def scalar: Rng[R]
+
+  def timesr(v: V, r: R): V
+}
+object RightModule {
+  @inline final def apply[V, @sp(Int,Long,Float,Double) R](implicit V: RightModule[V, R]): RightModule[V, R] = V
+
+  implicit def IdentityRightModule[@sp(Int,Long,Float,Double) V](implicit ring: Ring[V]): IdentityRightModule[V] = {
+    new IdentityRightModule[V] {
+      val scalar = ring
+    }
+  }
+}
 
 /**
  * A module generalizes a vector space by requiring its scalar need only form
  * a ring, rather than a field.
  */
-trait Module[V, @sp(Int,Long,Float,Double) R] extends Any with AdditiveAbGroup[V] {
-  implicit def scalar: Rng[R]
+trait Module[V, @sp(Int,Long,Float,Double) R] extends Any with LeftModule[R, V] with RightModule[V, R] {
+  implicit override def scalar: Rng[R]
 
   def timesl(r: R, v: V): V
-  def timesr(v: V, r: R): V = timesl(r, v)
+  override def timesr(v: V, r: R): V = timesl(r, v)
 }
 
 object Module {
@@ -30,6 +65,25 @@ private[algebra] trait IdentityModule[@sp(Int,Long,Float,Double) V] extends Any 
   override def minus(v: V, w: V): V = scalar.minus(v, w)
 
   def timesl(r: V, v: V): V = scalar.times(r, v)
+}
+
+private[algebra] trait IdentityLeftModule[@sp(Int,Long,Float,Double) V] extends Any with LeftModule[V, V] {
+  def zero: V = scalar.zero
+  def negate(v: V): V = scalar.negate(v)
+  def plus(v: V, w: V): V = scalar.plus(v, w)
+  override def minus(v: V, w: V): V = scalar.minus(v, w)
+
+  def timesl(r: V, v: V): V = scalar.times(r, v)
+}
+
+private[algebra] trait IdentityRightModule[@sp(Int,Long,Float,Double) V] extends Any with RightModule[V, V] {
+  def zero: V = scalar.zero
+  def negate(v: V): V = scalar.negate(v)
+  def plus(v: V, w: V): V = scalar.plus(v, w)
+  override def minus(v: V, w: V): V = scalar.minus(v, w)
+
+  def timesr(r: V, v: V): V = scalar.times(r, v)
+
 }
 
 final case class ZModule[V](vector: Group[V]) extends Module[V, Int] {
