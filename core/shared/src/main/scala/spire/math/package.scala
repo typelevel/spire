ceea40bd9f2efedd5b4171d0d7ecd7b1fe4eb158
Ryan Williams <ryan.blake.williams@gmail.com>
2018-02-14 16:12:13 +0000

    fix div by zero in Complex.norm

--- core/shared/src/main/scala/spire/math/package.scala
+++ core/shared/src/main/scala/spire/math/package.scala
@@ -404,7 +404,9 @@ package object math {
     (implicit f: Field[A], n: NRoot[A], s: Signed[A]): A = {
     import spire.implicits._
     def abs(n: A): A = if (n < f.zero) -n else n
-    if (x > y) abs(x) * (1 + (y/x)**2).sqrt
+    if (x == f.zero) y
+    else if (y == f.zero) x
+    else if (x > y) abs(x) * (1 + (y/x)**2).sqrt
     else abs(y) * (1 + (x/y)**2).sqrt
   }
 
