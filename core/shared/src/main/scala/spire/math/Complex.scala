f2a756405eee8554f32a567332e98adbe853bc2a
Ryan Williams <ryan.blake.williams@gmail.com>
2018-02-20 23:14:07 +0000

    cr feedback

--- core/shared/src/main/scala/spire/math/Complex.scala
+++ core/shared/src/main/scala/spire/math/Complex.scala
@@ -222,20 +222,22 @@ final case class Complex[@sp(Float, Double) T](real: T, imag: T)
   // returns the root with angle in (-pi/2, pi/2]
   def sqrt(implicit f: Field[T], n0: NRoot[T], s: Signed[T]): Complex[T] = {
     if (isZero) {
-      Complex.zero[T]
+      this
+    } else if (imag.isSignZero()) {
+      if (real.isSignNegative())
+        Complex(f.zero, real.abs.sqrt)
+      else
+        Complex(real.abs.sqrt, f.zero)
     } else {
+      // https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Negative_or_complex_square
       val two = f.fromInt(2)
+      val abs = this.abs
       val a = ((abs + real) / two).sqrt
-      imag.signum match {
-        case 0 =>
-          if (real < f.zero)
-            Complex(f.zero, real.abs.sqrt)
-          else
-            Complex(real.abs.sqrt, f.zero)
-        case n =>
-          val b = ((abs - real) / two).sqrt
-          if (n < 0) Complex(a, -b) else Complex(a, b)
-      }
+      val b = ((abs - real) / two).sqrt
+      if (imag.isSignNegative())
+        Complex(a, -b)
+      else
+        Complex(a, b)
     }
   }
 
