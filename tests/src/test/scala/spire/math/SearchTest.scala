bd495f897f2257385a4fcd6d801aa32117171eb1
jeniffer9 <jeniffer.limagraf@epfl.ch>
2018-12-15 15:42:01 +0100

    add search tests for IndexedSeq

--- tests/src/test/scala/spire/math/SearchTest.scala
+++ tests/src/test/scala/spire/math/SearchTest.scala
@@ -15,5 +15,16 @@ class SearchTest extends FunSuite {
     assert(Searching.search(Array("a", "b", "c"), "aaa") < 0)
     assert(Searching.search(Array("a", "b", "c"), "b") == 1)
   }
+
+  test("search indexedSeq when type provides a traditional comparison method") {
+    assert(Searching.search(IndexedSeq(1, 2, 3), 0) == -1)
+    assert(Searching.search(IndexedSeq(1, 2, 3), 2) == 1)
+  }
+
+  test("search indexedSeq when comparison can be a value not in {-1, 0, 1}") {
+    assert(Searching.search(IndexedSeq("a", "b", "c"), "aaa") < 0)
+    assert(Searching.search(IndexedSeq("a", "b", "c"), "b") == 1)
+  }
+
 }
 
