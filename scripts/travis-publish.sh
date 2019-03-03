d04cfcdec1f644cabbbbabf6ec8ae29af6387941
Erik Erlandson <eerlands@redhat.com>
2019-03-03 13:46:25 -0700

    take out JS testing to see if JVM tests run in CI

--- scripts/travis-publish.sh
+++ scripts/travis-publish.sh
@@ -10,6 +10,7 @@
 sbt_cmd="sbt ++${TRAVIS_SCALA_VERSION}!"
 scala_js="$sbt_cmd macrosJS/test && $sbt_cmd coreJS/test && $sbt_cmd extrasJS/test && $sbt_cmd lawsJS/test && $sbt_cmd testsJS/test"
 scala_jvm="$sbt_cmd clean coverage validateJVM coverageReport coverageOff docs/tut"
-run_cmd="$scala_js && $scala_jvm"
+#run_cmd="$scala_js && $scala_jvm"
+run_cmd="$scala_jvm"
 
 eval $run_cmd
