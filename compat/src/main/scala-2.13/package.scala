52d5a35313f8c12857143b8b083b5b5aced2bedd
Erik Erlandson <eerlands@redhat.com>
2019-03-04 07:17:10 -0700

    add explicit return type to parallelSeq shim

--- compat/src/main/scala-2.13/package.scala
+++ compat/src/main/scala-2.13/package.scala
@@ -7,7 +7,7 @@ import scala.reflect.ClassTag
   def arrayBuilderMake[T]()(implicit tag: ClassTag[T]): ArrayBuilder[T] =
       ArrayBuilder.make[T]
 
-  def parallelSeq[A](s: Seq[A]) = {
+  def parallelSeq[A](s: Seq[A]): Seq[A] = {
     println("parallel sequences are currently disabled for scala 2.13 and higher")
     s
   }
--- compat/src/main/scala-pre-2.13/package.scala
+++ compat/src/main/scala-pre-2.13/package.scala
@@ -2,12 +2,13 @@ package spire
 package object scalacompat {
 
 import scala.collection.mutable.ArrayBuilder
+import scala.collection.parallel.ParSeq
 import scala.reflect.ClassTag
 
   def arrayBuilderMake[T]()(implicit tag: ClassTag[T]): ArrayBuilder[T] =
       ArrayBuilder.make[T]()
 
-  def parallelSeq[A](s: Seq[A]) = s.par
+  def parallelSeq[A](s: Seq[A]): ParSeq[A] = s.par
 
   type SeqLike[A, C] = scala.collection.SeqLike[A, C]
 
