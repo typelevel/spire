b35990214c79a2feada38abaafd61a80e3ecce18
Stewart Stewart <stewinsalot@gmail.com>
2019-05-05 14:22:58 -0400

    Make `Perm` extend `(Int => Int)`
    - adds an `apply` method that replaces calls of `perm.mappping.getOrElse(n, n)`
    - adds smart `apply` constructor to enforce constraints for `Perm`
    - adds a cycle-based constructor

--- core/src/main/scala/spire/optional/Perm.scala
+++ core/src/main/scala/spire/optional/Perm.scala
@@ -14,17 +14,42 @@ import spire.util._
  * Represents a permutation encoded as a map from preimages to images, including
  * only pairs that are moved by the permutation (so the identity is Map.empty).
  */
-case class Perm(mapping: Map[Int, Int])
+class Perm private(val mapping: Map[Int, Int]) extends (Int => Int) {
+
+  override def apply(k: Int): Int = mapping.getOrElse(k, k)
+
+  def apply(n0: Int, n1: Int, ns: Int*): Perm = Group[Perm].combine(this, Perm(n0, n1, ns: _*))
+
+  override def toString: String = {
+    mapping.toSeq.sorted
+      .map { case (k, v) => s"$k -> $v"}
+      .mkString("Perm(", ", ", ")")
+  }
+}
 
 object Perm {
-  implicit val PermEq: Eq[Perm] = Eq.fromUniversalEquals
+  def apply(mapping: Map[Int, Int]): Perm = {
+    require(mapping.values.toSet == mapping.keySet, "Image and preimage must be the same.")
+    require(mapping.keys.forall(_ >= 0), "Perm indices must be non-negative.")
+    new Perm(mapping.filter(Function.tupled(_ != _)))
+  }
+
+  def apply(pairs: (Int, Int)*): Perm = apply(Map(pairs: _*))
+
+  def apply(n0: Int, n1: Int, ns: Int*): Perm = {
+    apply((n0 +: n1 +: ns).zip(n1 +: ns :+ n0).toMap)
+  }
+
+  implicit val PermEq: Eq[Perm] = new Eq[Perm] {
+    def eqv(p: Perm, r: Perm): Boolean = p.mapping == r.mapping
+  }
   implicit val PermIntAction: Action[Int, Perm] = new PermIntAction
   implicit val PermGroup: Group[Perm] = new PermGroup
   implicit def PermSeqPartialAction[A, CC[A] <: SeqLike[A, CC[A]]](implicit cbf: Factory[A, CC[A]]): PartialAction[CC[A], Perm] = new PermSeqPartialAction[A, CC[A]]
 }
 
 final class PermIntAction extends Action[Int, Perm] {
-  def actr(k: Int, perm: Perm): Int = perm.mapping.getOrElse(k, k)
+  def actr(k: Int, perm: Perm): Int = perm(k)
   def actl(perm: Perm, k: Int): Int = perm.mapping.find(_._2 == k).fold(k)(_._1)
 }
 
@@ -35,8 +60,7 @@ final class PermGroup extends Group[Perm] {
 
     preimages.foldLeft(Map.empty[Int, Int]) {
       case (prevMap, preimage) =>
-        val imageX = x.mapping.getOrElse(preimage, preimage)
-        val image = y.mapping.getOrElse(imageX, imageX)
+        val image = y(x(preimage))
         if (preimage != image) prevMap + ((preimage, image)) else prevMap
     }
   }
@@ -49,7 +73,7 @@ final class PermSeqPartialAction[A, SA <: SeqLike[A, SA]](implicit cbf: Factory[
     if (perm.mapping.keys.max >= sa.size) return Opt.empty[SA]
     val builder = cbf.newBuilder
     cforRange(0 until sa.size) { k =>
-      builder += sa(perm.mapping.getOrElse(k, k))
+      builder += sa(perm(k))
     }
     Opt(builder.result())
   }
