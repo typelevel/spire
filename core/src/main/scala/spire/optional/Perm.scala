9e382c739a8e76fdeed00cb39df6d831338b6419
Stewart Stewart <stewinsalot@gmail.com>
2019-05-18 18:45:42 -0400

    Add documentation to `Perm`

--- core/src/main/scala/spire/optional/Perm.scala
+++ core/src/main/scala/spire/optional/Perm.scala
@@ -9,14 +9,23 @@ import spire.algebra.partial.PartialAction
 import spire.syntax.cfor._
 import spire.util._
 
-/**
- * Represents a permutation encoded as a map from preimages to images, including
- * only pairs that are moved by the permutation (so the identity is Map.empty).
- */
+/** Represents a permutation encoded as a map from preimages to images, including
+  * only pairs that are moved by the permutation (so the identity is Map.empty).
+  *
+  * Note that although the preimage and image contain only moved points, the
+  * domain and range of a Perm is all integers. This acts as the identity for
+  * integers not in the image.
+  */
 class Perm private(private val mapping: Map[Int, Int]) extends (Int => Int) {
 
+  /** Apply this permutation to an `Int`. */
   override def apply(k: Int): Int = mapping.getOrElse(k, k)
 
+  /** A `Perm` constructed by cycling args so each n,,i,, maps to n,,i+1,,.
+    *
+    * This new cycle is composed with the current permutation to yield a new
+    * `Perm` with cycle applied last. See [[Perm$.apply(n0:Int*]].
+    */
   def apply(n0: Int, n1: Int, ns: Int*): Perm = this.andThen(Perm(n0, n1, ns: _*))
 
   override def toString: String = {
@@ -27,12 +36,19 @@ class Perm private(private val mapping: Map[Int, Int]) extends (Int => Int) {
 
   private lazy val inverseMapping = mapping.map(_.swap)
 
+  /** The preimage of `k` (so that `apply(invert(k)) == k`). */
   def invert(k: Int): Int = inverseMapping.getOrElse(k, k)
 
+  /** The inverse permutation, which composes with this to yield the identity.
+    * Note that this uses a lazy inverse map, so the first call may be O(n)
+    * rather than O(1).
+    */
   def inverse: Perm = new Perm(inverseMapping)
 
+  /** The set of points moved by this permutation (equivalent to preimage). */
   def image: Set[Int] = mapping.keySet
 
+  /** Permute a seq as long as all the moved points are valid indices. */
   def permute[A, SA](seq: SeqLike[A, SA])(implicit cbf: Factory[A, SA]): Opt[SA] = {
     if (image.isEmpty) return Opt(cbf.fromSpecific(seq))
     if (image.max >= seq.size) return Opt.empty[SA]
@@ -43,6 +59,7 @@ class Perm private(private val mapping: Map[Int, Int]) extends (Int => Int) {
     Opt(builder.result())
   }
 
+  /** Compose this with another `Perm` with this permutation applied last. */
   def compose(that: Perm): Perm = new Perm(
     (this.image | that.image)
       .map(k => k -> this(that(k)))
@@ -50,18 +67,29 @@ class Perm private(private val mapping: Map[Int, Int]) extends (Int => Int) {
       .toMap
   )
 
+  /** Compose this with another `Perm` with this permutation applied first. */
   def andThen(that: Perm): Perm = that.compose(this)
 }
 
 object Perm {
+  /** A `Perm` given a `Map[Int, Int]` from preimage to image. */
   def apply(mapping: Map[Int, Int]): Perm = {
     require(mapping.values.toSet == mapping.keySet, "Image and preimage must be the same.")
     require(mapping.keys.forall(_ >= 0), "Perm indices must be non-negative.")
     new Perm(mapping.filter(Function.tupled(_ != _)))
   }
 
+  /** A `Perm` given a collection of preimage/image pairs. */
   def apply(pairs: (Int, Int)*): Perm = apply(Map(pairs: _*))
 
+  /** A `Perm` constructed by cycling args so each n,,i,, maps to n,,i+1,,.
+    *
+    * [[Perm#apply(n0:Int*]] can be called subsequently to express any `Perm` as
+    * the product of cycles rather than explicit preimage/image pairs. This is
+    * provided as a convenience for constructing permutations. eg.,
+    * `Perm(1,3)(2,4)` is shorthand for `Perm(1 -> 3, 2 -> 4, 3 -> 1, 4 ->
+    * 2)`. At least two args are required in each parameter list.
+    */
   def apply(n0: Int, n1: Int, ns: Int*): Perm = {
     apply((n0 +: n1 +: ns).zip(n1 +: ns :+ n0).toMap)
   }
