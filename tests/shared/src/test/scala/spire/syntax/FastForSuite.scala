15134dbbf1fa04c2171500516282b093dd3f09e4
Arman Bilge <armanbilge@gmail.com>
2021-10-17 04:20:41 +0000

    Add adjusted closure test to FastForSuite

--- tests/shared/src/test/scala/spire/syntax/FastForSuite.scala
+++ tests/shared/src/test/scala/spire/syntax/FastForSuite.scala
@@ -84,6 +84,19 @@ class FastForSuite extends munit.FunSuite {
     assertEquals(b.toList, List(0, 1, 2))
   }
 
+  // This test distinguishes fastFor from cfor
+  test("doesn't capture value in closure") {
+    val b1 = collection.mutable.ArrayBuffer.empty[() => Int]
+    fastFor(0)(_ < 3, _ + 1) { x =>
+      b1 += (() => x)
+    }
+    val b2 = collection.mutable.ArrayBuffer[() => Int]()
+    (0 until 3).foreach { x =>
+      b2 += (() => x)
+    }
+    assertEquals(b1.map(_.apply()).toList, b2.map(_.apply()).toList)
+  }
+
   test("capture value in inner class") {
     val b = collection.mutable.ArrayBuffer[Int]()
     fastFor(0)(_ < 3, _ + 1) { x =>
