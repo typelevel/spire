c30c20341996deafc3f523161f9a02f73f530827
Carlos Quiroz <3615303+cquiroz@users.noreply.github.com>
2021-10-18 21:44:59 -0300

    Make the scala-3 unicode package use extension methods
    Signed-off-by: Carlos Quiroz <3615303+cquiroz@users.noreply.github.com>

--- core/src/main/scala-3/spire/optional/unicode.scala
+++ core/src/main/scala-3/spire/optional/unicode.scala
@@ -24,52 +24,45 @@ val φ = Real.phi
 val ⅈ = Complex.i[Real]
 val ⅉ = Quaternion.j[Real]
 
-def ⊤[A](implicit ev: Heyting[A]): A = ev.one
-def ⊥[A](implicit ev: Heyting[A]): A = ev.zero
-def ¬[A](a: A)(implicit ev: Heyting[A]): A = ev.complement(a)
-def √[A](a: A)(implicit ev: NRoot[A]): A = ev.sqrt(a)
-def ∛[A](a: A)(implicit ev: NRoot[A]): A = ev.nroot(a, 3)
-def ∜[A](a: A)(implicit ev: NRoot[A]): A = ev.nroot(a, 4)
+def ⊤[A](using ev: Heyting[A]): A = ev.one
+def ⊥[A](using ev: Heyting[A]): A = ev.zero
+def ¬[A](a: A)(using ev: Heyting[A]): A = ev.complement(a)
+def √[A](a: A)(using ev: NRoot[A]): A = ev.sqrt(a)
+def ∛[A](a: A)(using ev: NRoot[A]): A = ev.nroot(a, 3)
+def ∜[A](a: A)(using ev: NRoot[A]): A = ev.nroot(a, 4)
 
-def Σ[A](as: Iterable[A])(implicit ev: AdditiveMonoid[A]): A =
+def Σ[A](as: Iterable[A])(using ev: AdditiveMonoid[A]): A =
   as.foldLeft(ev.zero)(ev.plus)
 
-def Π[A](as: Iterable[A])(implicit ev: MultiplicativeMonoid[A]): A =
+def Π[A](as: Iterable[A])(using ev: MultiplicativeMonoid[A]): A =
   as.foldLeft(ev.one)(ev.times)
 
-implicit class TimesOp[A](lhs: A)(implicit ev: MultiplicativeSemigroup[A]) {
+extension[A](lhs: A)(using ev: MultiplicativeSemigroup[A])
   def ∙(rhs: A): A = ev.times(lhs, rhs)
-}
 
-implicit class EqOps[A](lhs: A)(implicit ev: Eq[A]) {
+extension[A](lhs: A)(using ev: Eq[A])
   def ≡(rhs: A): Boolean = ev.eqv(lhs, rhs)
   def ≠(rhs: A): Boolean = ev.neqv(lhs, rhs)
-}
 
-implicit class PartialOrderOps[A](lhs: A)(implicit ev: PartialOrder[A]) {
+extension[A](lhs: A)(using ev: PartialOrder[A])
   def ≤(rhs: A): Boolean = ev.lteqv(lhs, rhs)
   def ≥(rhs: A): Boolean = ev.gteqv(lhs, rhs)
-}
 
-implicit class MeetSemilatticeOps[A](lhs: A)(implicit ev: MeetSemilattice[A]) {
+extension[A](lhs: A)(using ev: MeetSemilattice[A])
   def ∧(rhs: A): A = ev.meet(lhs, rhs)
-}
 
-implicit class JoinSemilatticeOps[A](lhs: A)(implicit ev: JoinSemilattice[A]) {
+extension[A](lhs: A)(using ev: JoinSemilattice[A])
   def ∨(rhs: A): A = ev.join(lhs, rhs)
-}
 
-implicit class HeytingOps[A](lhs: A)(implicit ev: Heyting[A]) {
+extension[A](lhs: A)(using ev: Heyting[A])
   def ⊃(rhs: A): A = ev.imp(lhs, rhs)
-}
 
-implicit class BoolOps[A](lhs: A)(implicit ev: Bool[A]) {
+extension[A](lhs: A)(using ev: Bool[A])
   def ⊻(rhs: A): A = ev.xor(lhs, rhs)
   def ⊼(rhs: A): A = ev.nand(lhs, rhs)
   def ⊽(rhs: A): A = ev.nor(lhs, rhs)
-}
 
-implicit class SymbolicSetOps[A](val lhs: Set[A]) extends AnyVal {
+extension[A](lhs: Set[A])
   def ∋(a: A): Boolean = lhs(a)
   def ∌(a: A): Boolean = !lhs(a)
 
@@ -85,4 +78,3 @@ implicit class SymbolicSetOps[A](val lhs: Set[A]) extends AnyVal {
 
   def ⊆(rhs: Set[A]): Boolean = lhs.size <= rhs.size && lhs.forall(rhs)
   def ⊇(rhs: Set[A]): Boolean = lhs.size >= rhs.size && rhs.forall(lhs)
-}
--- tests/shared/src/test/scala/spire/optional/unicode/UnicodeSuite.scala
+++ tests/shared/src/test/scala/spire/optional/unicode/UnicodeSuite.scala
@@ -6,5 +6,6 @@ class UnicodeSuite extends munit.FunSuite {
   test("Sanity test") {
     // Basically we want to ensure the module is present in scala-2 and scala-3
     assertEquals(√(4.0), 2.0)
+    assert(2.00 ≡ 2.00)
   }
 }
