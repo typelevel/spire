2c9ad1b69811bc4612560ff3eaa735a16f9a6a4c
Carlos Quiroz <3615303+cquiroz@users.noreply.github.com>
2021-10-25 12:36:38 -0300

    Alternative encoding type aliasing NoImplicit
    Signed-off-by: Carlos Quiroz <3615303+cquiroz@users.noreply.github.com>

---
+++ core/src/main/scala-2/spire/package.scala
@@ -0,0 +1,20 @@
+/*
+ * **********************************************************************\
+ * * Project                                                              **
+ * *       ______  ______   __    ______    ____                          **
+ * *      / ____/ / __  /  / /   / __  /   / __/     (c) 2011-2021        **
+ * *     / /__   / /_/ /  / /   / /_/ /   / /_                            **
+ * *    /___  / / ____/  / /   / __  /   / __/   Erik Osheim, Tom Switzer **
+ * *   ____/ / / /      / /   / / | |   / /__                             **
+ * *  /_____/ /_/      /_/   /_/  |_|  /____/     All rights reserved.    **
+ * *                                                                      **
+ * *      Redistribution and use permitted under the MIT license.         **
+ * *                                                                      **
+ * \***********************************************************************
+ */
+
+package object spire {
+  type ClassTag[A] = scala.reflect.ClassTag[A]
+  val ClassTag = scala.reflect.ClassTag
+  type NotGiven[A] = spire.NoImplicit[A]
+}
