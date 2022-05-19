cf4c5415b5b0bbd4893c454e6ba5bc9bc46c5c53
kschwarz1116 <kevin.schwarz@disneystreaming.com>
2022-05-19 00:59:29 -0400

    revert fractionalsuite change, use local implicits

--- tests/shared/src/test/scala/spire/math/RealScalaCheckSuite.scala
+++ tests/shared/src/test/scala/spire/math/RealScalaCheckSuite.scala
@@ -18,6 +18,7 @@ package math
 
 import spire.implicits._
 import spire.laws.arb.{rational, real}
+import spire.laws.gen.realFromLongs
 
 import ArbitrarySupport._
 import Ordinal._
@@ -114,6 +115,33 @@ class RealScalaCheckSuite extends munit.ScalaCheckSuite {
   property("x.pow(3) = x * x * x") {
     forAll { (x: Real) =>
       x.pow(3) == x * x * x
+    }
+  }
+
+  val realSizedArb: Arbitrary[(Real, Sized[Int, _1, _10])] =
+    Arbitrary.arbTuple2(Arbitrary(realFromLongs), Arbitrary(Arbitrary.arbitrary[Sized[Int, _1, _10]]))
+
+  property("x.pow(k).nroot(k) = x") {
+    forAll(realSizedArb.arbitrary){ (x0: Real, k: Sized[Int, _1, _10]) =>
+      val x = x0.abs
+      x.pow(k.num).nroot(k.num) == x
+    }
+  }
+
+  property("x.nroot(k).pow(k) = x") {
+    forAll(realSizedArb.arbitrary){ (x0: Real, k: Sized[Int, _1, _10]) =>
+      val x = x0.abs
+      x.nroot(k.num).pow(k.num) == x
+    }
+  }
+
+  property("x.nroot(-k).pow(-k) = x") {
+    forAll(
+      nonZeroSpireImplicit[Real](implicitly, implicitly, Arbitrary(realFromLongs)).arbitrary,
+      Arbitrary.arbitrary[Sized[Int, _1, _10]]){
+      (x0: NonZero[Real], k: Sized[Int, _1, _10]) =>
+      val x = x0.num.abs
+      x.nroot(-k.num).pow(-k.num) == x
     }
   }
 
