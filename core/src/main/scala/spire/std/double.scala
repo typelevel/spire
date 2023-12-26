f8a8be66fc37a633fe86dcde7b4d6199bc039554
Erik Erlandson <eerlands@redhat.com>
2023-12-26 14:33:20 -0700

    DoubleAlgebra is shared again

---
+++ core/src/main/scala/spire/std/double.scala
@@ -0,0 +1,40 @@
+/*
+ * **********************************************************************\
+ * * Project                                                              **
+ * *       ______  ______   __    ______    ____                          **
+ * *      / ____/ / __  /  / /   / __  /   / __/     (c) 2011-2021        **
+ * *     / /__   / /_/ /  / /   / /_/ /   / /_                            **
+ * *    /___  / / ____/  / /   / __  /   / __/   Erik Osheim, Tom Switzer **
+ * *   ____/ / / /      / /   / / | |   / /__                             **
+ * *  /_____/ /_/      /_/   /_/  |_|  /____/     All rights reserved.    **
+ * *                                                                      **
+ * *      Redistribution and use permitted under the MIT license.         **
+ * *                                                                      **
+ * \***********************************************************************
+ */
+
+package spire
+package std
+
+import spire.algebra.{Field, IsRational, NRoot, Order, Signed, Trig, TruncatedDivisionCRing}
+
+@SerialVersionUID(0L)
+class DoubleAlgebra extends DoubleIsField with DoubleIsNRoot with DoubleIsTrig with DoubleIsReal with Serializable
+
+trait DoubleInstances {
+  implicit final val DoubleAlgebra: Field[Double]
+    with NRoot[Double]
+    with Trig[Double]
+    with IsRational[Double]
+    with TruncatedDivisionCRing[Double]
+    with Signed[Double]
+    with Order[Double] = new DoubleAlgebra
+  import Double._
+  import spire.math.NumberTag
+  import spire.math.NumberTag._
+  implicit final val DoubleTag: NumberTag[Double] =
+    new BuiltinFloatTag(0d, MinValue, MaxValue, NaN, PositiveInfinity, NegativeInfinity) {
+      def isInfinite(a: Double): Boolean = java.lang.Double.isInfinite(a)
+      def isNaN(a: Double): Boolean = java.lang.Double.isNaN(a)
+    }
+}
